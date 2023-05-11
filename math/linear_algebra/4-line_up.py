6cb936bcc45991f0725073e331fd90b4867aa8ba
Your Name <you@example.com>
2023-05-11 15:01:01 -0500

    a function def add_arrays(arr1, arr2): that adds two arrays element-wise

--- math/linear_algebra/4-line_up.py
+++ math/linear_algebra/4-line_up.py
@@ -4,7 +4,7 @@
 
 def add_arrays(arr1, arr2):  # defines a function named add_arrays with 2 para
     """ Two arrays add and returns """
-    if len(arr1) != len(arr2):  # checks if the lengths of arr1 and arr2 
+    if len(arr1) != len(arr2):  # checks if the lengths of arr1 and arr2
         return None
 
     result = []
