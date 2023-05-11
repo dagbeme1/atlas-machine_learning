75e26be2eac973dc2b3fa53e5cfc2f0cad4da9cb
Your Name <you@example.com>
2023-05-11 16:11:47 -0500

    a function def cat_matrices2D(mat1, mat2, axis=0): that concatenates two matrices along a specific axis

---
+++ math/linear_algebra/7-gettin_cozy.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+""" concat matrix """
+
+
+def cat_matrices2D(mat1, mat2, axis=0):
+    """ concat matrix based on axis """
+    if axis == 0:
+        if len(mat1[0]) != len(mat2[0]):
+            return None
+        return mat1 + mat2
+    elif axis == 1:
+        if len(mat1) != len(mat2):
+            return None
+        return [row1 + row2 for row1, row2 in zip(mat1, mat2)]
+    else:
+        return None
+
