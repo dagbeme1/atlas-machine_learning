d081fcc2d9e2529723ba3be979f81207ab333e0b
Your Name <you@example.com>
2023-05-11 17:38:51 -0500

    a function def mat_mul(mat1, mat2): that performs matrix multiplication

--- math/linear_algebra/8-ridin_bareback.py
+++ math/linear_algebra/8-ridin_bareback.py
@@ -7,14 +7,14 @@ def mat_mul(mat1, mat2):
     # Check if the matrices can be multiplied
     if len(mat1[0]) != len(mat2):
         return None
-                        
+
     # Create an empty result matrix
     result = [[0] * len(mat2[0]) for _ in range(len(mat1))]
-                                
+
     # Perform matrix multiplication
     for i in range(len(mat1)):
         for j in range(len(mat2[0])):
             for k in range(len(mat2)):
                 result[i][j] += mat1[i][k] * mat2[k][j]
-                                                                                            
+                                 
     return result
