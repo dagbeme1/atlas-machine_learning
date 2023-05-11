e7ca66312d865a27843201f90d3f8e5d1a4a3ffe
Your Name <you@example.com>
2023-05-11 15:06:54 -0500

    a function def add_arrays(arr1, arr2): that adds two arrays element-wise

--- math/linear_algebra/4-line_up.py
+++ math/linear_algebra/4-line_up.py
@@ -2,13 +2,13 @@
 """ matrix addition of arrays  """
 
 
-def add_arrays(arr1, arr2):  # defines a function named add_arrays with 2 para
+def add_arrays(arr1, arr2):  # defines a function named add_arrays with 2 parameters arr1 and arr2
     """ Two arrays add and returns """
-    if len(arr1) != len(arr2):  # checks if the lengths of arr1 and arr2
+    if len(arr1) != len(arr2):  # checks if the lengths of arr1 and arr2 are not equal. If they have different lengths, and cannot add them
         return None
 
-    result = []
+    result = []  # store the element-wise addition of the arrays
     for i in range(len(arr1)):
-        result.append(arr1[i] + arr2[i])
+        result.append(arr1[i] + arr2[i])  #  iterates over the indices of arr1 (or arr2 since they have the same length) using the range function
 
     return result
