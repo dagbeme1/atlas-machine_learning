6c5a598d48c0fd04d66e6072ed3dddabf677dba6
Your Name <you@example.com>
2023-05-11 13:58:25 -0500

    a function def matrix_shape(matrix): that calculates the shape of a matrix

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python3
-""" calculate the shape of a matrix using nested loops """
-def matrix_shape(matrix):  # parameter(matrix)
-        shape = []  # an empty list.stores dimensions of the matrix
-        while isinstance(matrix, list):  
-            shape.append(len(matrix))  # iteration of the outer loop, length
-            matrix = matrix[0]
-        return shape[::-1]  #  list in reverse order,matrix from outermost to innermost
+"""Write a function that calculates the shape of a matrix"""
 
+
+def matrix_shape(matrix):
+    """Return shape of given list-matrix"""
+    shape = []
+    try:
+        while(len(matrix) > 0):
+            shape.append(len(matrix))
+            matrix = matrix[0]
+    except TypeError:
+        pass
+    return shape
