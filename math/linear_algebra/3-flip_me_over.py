5e0c8722043a19ac1138fdb3f8dae6af0e12248c
Your Name <you@example.com>
2023-05-11 14:27:33 -0500

    a function def matrix_transpose(matrix): that returns the transpose of a 2D matrix, matrix

---
+++ math/linear_algebra/3-flip_me_over.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+""" Transpose matrix """
+
+def matrix_transpose(matrix):
+    """ Given matrix perform transpose"""
+    new_transpose = []
+    for idx in range(len(matrix[0])):
+        current = []
+        for row in matrix:
+            current.append(row[idx])
+        new_transpose.append(current)
+    return new_transpose
