60a7262d686d9aff0f3ea65a9fdf825464702f40
Your Name <you@example.com>
2023-05-11 15:36:42 -0500

    a function def add_matrices2D(mat1, mat2): that adds two matrices element-wise

--- math/linear_algebra/5-across_the_planes.py
+++ math/linear_algebra/5-across_the_planes.py
@@ -4,13 +4,13 @@
 
 def add_matrices2D(mat1, mat2):
     """ add 2 matrices or return """
-    if len(mat1) != len(mat2) or len(mat1[0]) != len(mat2[0]):
+    if len(mat1) != len(mat2) or len(mat1[0]) != len(mat2[0]):  # checks if the number of rows and number of columns of mat1 and mat2 are not the same
         return None
 
     result = []
     for i in range(len(mat1)):
         row = []
-        for j in range(len(mat1[0])):
+        for j in range(len(mat1[0])):  # iterates over the rows and columns of mat1 (or mat2 since they have the same shape) using nested loops
             row.append(mat1[i][j] + mat2[i][j])
         result.append(row)
 
