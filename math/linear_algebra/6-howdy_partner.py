b8a25385357966012ef5f0bdfad084b32ef0e5da
Your Name <you@example.com>
2023-05-11 15:58:56 -0500

    a function def cat_arrays(arr1, arr2): that concatenates two arrays

--- math/linear_algebra/6-howdy_partner.py
+++ math/linear_algebra/6-howdy_partner.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """ concat two arrays """
 
+
 def cat_arrays(arr1, arr2):
-    """ defines a function named cat_arrays that takes two parameters arr1 and arr2 """
+    """ defines a function named cat_arrays that takes two parameters """
     return arr1 + arr2
