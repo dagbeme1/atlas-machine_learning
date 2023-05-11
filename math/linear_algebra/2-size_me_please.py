bc134c00cd913fcd26496f7f076e45f3291b3a04
Your Name <you@example.com>
2023-05-11 14:08:02 -0500

    a function def matrix_shape(matrix): that calculates the shape of a matrix

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -4,11 +4,11 @@
 
 def matrix_shape(matrix):
     """Return shape of given list-matrix"""
-    shape = []
-    try:
-        while(len(matrix) > 0):
-            shape.append(len(matrix))
-            matrix = matrix[0]
-    except TypeError:
+    shape = []  # empty list which stores dimensions of the matrix
+    try:  # a block of code that we want to try running, and if there's an error, we can handle it later.
+        while(len(matrix) > 0):  # continues loop as long as length as matrix is greater than zero
+            shape.append(len(matrix))  # adds the length of the current dimension to the shape list
+            matrix = matrix[0]  # updates the matrix to the first element of the current dimension
+    except TypeError:  # something goes wrong and it's a TypeError, we can handle it without the program crashing
         pass
     return shape
