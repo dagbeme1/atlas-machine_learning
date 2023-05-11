f3dfb5426a2720568fc8f50e203f8d334b8e5425
Your Name <you@example.com>
2023-05-11 16:26:05 -0500

    a function def np_matmul(mat1, mat2): that performs matrix multiplication

---
+++ math/linear_algebra/14-saddle_up.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" matrix mult"""
+import numpy as np  # imports the NumPy library and makes it accessible
+
+
+def np_matmul(mat1, mat2):  # defines the function np_matmul
+    """ matrix multiplication"""
+    return np.matmul(mat1, mat2)
