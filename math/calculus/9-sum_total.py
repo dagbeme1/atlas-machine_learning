d1ad85f9617e3f3019318bd0ad4cbf84ad19526b
Your Name <you@example.com>
2023-05-17 18:03:59 -0500

    a function def summation_i_squared(n): that calculates \sum_{i=1}^{n} i^2

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
-"""sum squared"""
-
+""" sum squared """
 
 def summation_i_squared(n):
-    """returns sum of n squared"""
-    if n < 1 or type(n) != int:
+    if not isinstance(n, int) or n <= 0:
         return None
-        return sum(map(lambda n: pow(n, 2), range(n + 1)))
+
+    sum_of_squares = sum(i ** 2 for i in range(1, n+1))
+    return sum_of_squares
