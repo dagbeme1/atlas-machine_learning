e74b3b8a9e883683edd340dd0267c098c63d161f
Your Name <you@example.com>
2023-05-22 19:14:46 -0500

    Instance method def cdf(self, k)

--- math/probability/poisson.py
+++ math/probability/poisson.py
@@ -93,3 +93,23 @@ class Poisson:
         den = self.factorial(k)
 
         return num / den
+
+    def cdf(self, k):
+        """Calculates Cumulative Distribution Function (CDF)
+
+        Args:
+            k: number of successes
+
+        Returns:
+            CDF of k or 0 if k is out of range.
+        """
+        k = int(k)
+
+        if k < 0:
+            return 0
+
+        summation = 0
+        for i in range(k + 1):
+            summation += (self.lambtha ** i) / (self.factorial(i))
+
+        return (self.EULER_NUMBER ** -self.lambtha) * summation
