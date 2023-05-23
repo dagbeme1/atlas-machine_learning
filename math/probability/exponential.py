bb5c351903973b6e4276f3082bf68870ac86c469
Your Name <you@example.com>
2023-05-22 19:56:42 -0500

    more comments added for better understanding

--- math/probability/exponential.py
+++ math/probability/exponential.py
@@ -9,13 +9,23 @@ class Exponential:
 
     def __init__(self, data=None, lambtha=1.):
         """Class constructor"""
+        # checks for the case data
         if data is not None:
+            # checks if data is a list
             if not isinstance(data, list):
                 raise TypeError('data must be a list')
+
+            # checks for data containing at least 2 values
             if len(data) < 2:
                 raise ValueError('data must contain multiple values')
+
+            # calculating for lambtha value
             self.lambtha = 1/(sum(data)/len(data))
         else:
+            # handles case when data is not provided
+            # checking if the lambtha is a positivee value
             if lambtha <= 0:
                 raise ValueError('lambtha must be a positive value')
+
+            # Assigning the lambtha value as a float
             self.lambtha = float(lambtha)
