8e2e38cd3f51f39529bd25d6c7698eb417390d2a
Dennis Agbemenu <105249635+dagbeme1@users.noreply.github.com>
2023-05-22 19:43:43 -0500

    Class contructor def __init__(self, data=None, lambtha=1.): data is a list of the data to be used to estimate the distribution lambtha is the expected number of occurences in a given time frame Sets the instance attribute lambtha

--- math/probability/exponential.py
+++ math/probability/exponential.py
@@ -3,44 +3,19 @@
 
 
 class Exponential:
+    """Class that represents an exponential distribution"""
 
     EULER_NUMBER = 2.7182818285
-    
+
     def __init__(self, data=None, lambtha=1.):
-        """
-        Initialize an Exponential distribution.
-
-        Args:
-            data: List of data to estimate the distribution (default: None).
-            lambtha: Expected number of occurrences in a given time frame.
-
-        Raises:
-            ValueError: If lambtha is not a positive value.
-            TypeError: If data is not a list.
-            ValueError: If data does not contain at least two data points.
-        """
-        if data is None:
-            if lambtha <= 0:
-                raise ValueError("lambtha must be a positive value")
-            self.lambtha = float(lambtha)
+        """Class constructor"""
+        if data is not None:
+            if not isinstance(data, list):
+                raise TypeError('data must be a list')
+            if len(data) < 2:
+                raise ValueError('data must contain multiple values')
+            self.lambtha = 1/(sum(data)/len(data))
         else:
-            self.set_lambtha(data)
-
-    def set_lambtha(self, data):
-        """
-        Set the lambtha attribute based on the given data.
-
-        Args:
-            data: List of data points.
-
-        Raises:
-            TypeError: If data is not a list.
-            ValueError: If data does not contain at least two data points.
-        """
-        if not isinstance(data, list):
-            raise TypeError("data must be a list")
-        if len(data) < 2:
-            raise ValueError("data must contain multiple values")
-        total = sum(data)
-        count = len(data)
-        self.lambtha = float(count) / total
+            if lambtha <= 0:
+                raise ValueError('lambtha must be a positive value')
+            self.lambtha = float(lambtha)
