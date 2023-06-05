bd575772561a7c2a8ee0cff41b64cb282e6b332e
Dennis Agbemenu <105249635+dagbeme1@users.noreply.github.com>
2023-06-04 22:39:54 -0500

    a class Neuron that defines a single neuron performing binary classification (Based on 1-neuron.py):
    Add the public method def forward_prop(self, X):
    Calculates the forward propagation of the neuron
    X is a numpy.ndarray with shape (nx, m) that contains the input data
    nx is the number of input features to the neuron
    m is the number of examples
    Updates the private attribute __A
    The neuron should use a sigmoid activation function
    Returns the private attribute __A

---
+++ supervised_learning/classification/2-neuron.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+"""Neuron class"""
+import numpy as np
+
+
+class Neuron:
+    """Class that defines a single neuron performing binary classification"""
+
+    def __init__(self, nx):
+        """Class constructor"""
+        if type(nx) is not int:
+            raise TypeError('nx must be an integer')  # Raise TypeError if nx is not an integer
+        if nx < 1:
+            raise ValueError('nx must be a positive integer')  # Raise ValueError if nx is less than 1
+        self.__W = np.random.randn(1, nx)  # Initialize W with random values from a normal distribution
+        self.__b = 0  # Initialize b to 0
+        self.__A = 0  # Initialize A to 0
+
+    @property
+    def W(self):
+        """W getter"""
+        return self.__W
+
+    @property
+    def b(self):
+        """b getter"""
+        return self.__b
+
+    @property
+    def A(self):
+        """A getter"""
+        return self.__A
+
+    def forward_prop(self, X):
+        """Calculates the forward propagation of the neuron
+
+        Args:
+            X: input data
+
+        Returns:
+            Activation function - calculated with sigmoid function
+        """
+        A_prev = np.matmul(self.__W, X) + self.__b  # Perform matrix multiplication of W and X, and add b
+        self.__A = 1 / (1 + np.exp(-A_prev))  # Apply sigmoid activation function
+        return self.__A
