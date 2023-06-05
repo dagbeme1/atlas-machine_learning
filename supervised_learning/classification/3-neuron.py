9dc24bddc89e297a57def2316a2628660e2b6ddb
Dennis Agbemenu <105249635+dagbeme1@users.noreply.github.com>
2023-06-04 23:06:35 -0500

    a class Neuron that defines a single neuron performing binary classification (Based on 2-neuron.py):
    Add the public method def cost(self, Y, A):
    Calculates the cost of the model using logistic regression
    Y is a numpy.ndarray with shape (1, m) that contains the correct labels for the input data
    A is a numpy.ndarray with shape (1, m) containing the activated output of the neuron for each example
    To avoid division by zero errors, please use 1.0000001 - A instead of 1 - A
    Returns the cost

--- supervised_learning/classification/3-neuron.py
+++ supervised_learning/classification/3-neuron.py
@@ -1,40 +1,35 @@
-#!/usr/bin/env python3  # Shebang line specifying the interpreter to use
+#!/usr/bin/env python3
+"""Neuron class"""
+import numpy as np
 
-"""Neuron class"""  # Docstring providing a brief description of the code
-
-import numpy as np  # Importing the numpy library for numerical computations
 
 class Neuron:
     """Class that defines a single neuron performing binary classification"""
-    # Docstring explaining the purpose of the class
 
     def __init__(self, nx):
         """Class constructor"""
         if type(nx) is not int:
             raise TypeError('nx must be an integer')
-        # Check if nx is an integer, raise an exception if not
         if nx < 1:
             raise ValueError('nx must be a positive integer')
-        # Check if nx is a positive integer, raise an exception if not
         self.__W = np.random.randn(1, nx)
-        # Initialize the weights with random values
-        self.__b = 0  # Initialize the bias to 0
-        self.__A = 0  # Initialize the activated output to 0
+        self.__b = 0
+        self.__A = 0
 
     @property
     def W(self):
-        """W getter"""  # Docstring explaining the purpose of the getter meth
-        return self.__W  # Return the value of the private attribute W
+        """W getter"""
+        return self.__W
 
     @property
     def b(self):
-        """b getter"""  # Docstring explaining the purpose of the getter meth
-        return self.__b  # Return the value of the private attribute b
+        """b getter"""
+        return self.__b
 
     @property
     def A(self):
-        """A getter"""  # Docstring explaining the purpose of the getter meth
-        return self.__A  # Return the value of the private attribute A
+        """A getter"""
+        return self.__A
 
     def forward_prop(self, X):
         """Calculates the forward propagation of the neuron
@@ -44,12 +39,10 @@ class Neuron:
 
         Returns:
             Activation function - calculated with sigmoid function
-        """  # Docstring explaining the purpose of the forward_prop method
+        """
         A_prev = np.matmul(self.__W, X) + self.__b
-        # Weighted sum of inputs and bias
         self.__A = 1 / (1 + np.exp(-A_prev))
-        # Apply sigmoid activation function
-        return self.__A  # Return the activated output
+        return self.__A
 
     def cost(self, Y, A):
         """Calculates the cost of the model using logistic regression
@@ -60,9 +53,8 @@ class Neuron:
 
         Returns:
             The cost
-        """  # Docstring explaining the purpose of the cost method
-        m = Y.shape[1]  # Number of training examples
+        """
+        m = Y.shape[1]
         cost = - (1 / m) * np.sum(np.multiply(Y, np.log(A)) +
                                   np.multiply(1 - Y, np.log(1.0000001 - A)))
-        # Compute cost using logistic regression formula
-        return cost  # Return the computed cost
+        return cost
