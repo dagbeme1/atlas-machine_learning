16276b7108f67f81263b3be9aba8cfd50fca3d62
Dennis Agbemenu <105249635+dagbeme1@users.noreply.github.com>
2023-06-07 18:52:06 -0500

    a function def one_hot_encode(Y, classes): that converts a numeric label vector into a one-hot matrix:
    Y is a numpy.ndarray with shape (m,) containing numeric class labels
    m is the number of examples
    classes is the maximum number of classes found in Y
    Returns: a one-hot encoding of Y with shape (classes, m), or None on failure

---
+++ supervised_learning/classification/24-one_hot_encode.py
@@ -0,0 +1,33 @@
+import numpy as np
+
+def one_hot_encode(Y, classes):
+    """
+    Converts a numeric label vector into a one-hot matrix.
+
+    Args:
+        Y: numpy.ndarray with shape (m,) containing numeric class labels.
+        classes: maximum number of classes found in Y.
+
+    Returns:
+        One-hot encoding of Y with shape (classes, m), or None on failure.
+    """
+    # Check if Y is a valid numpy array with shape (m,)
+    if not isinstance(Y, np.ndarray) or Y.ndim != 1:
+        return None
+
+    # Get the number of examples (m)
+    m = Y.shape[0]
+
+    # Create an array of zeros with shape (classes, m)
+    one_hot = np.zeros((classes, m))
+
+    # Iterate over the label vector Y
+    for i, label in enumerate(Y):
+        # Check if the label is within the valid range
+        if label < 0 or label >= classes:
+            return None
+        # Set the corresponding element in one_hot to 1
+        one_hot[label, i] = 1
+
+    # Return the resulting one-hot encoding
+    return one_hot
