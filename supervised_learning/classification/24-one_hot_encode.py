fb2034b6c1107640bd69b8fc9f2fa162d04124bc
Your Name <you@example.com>
2023-06-07 19:14:42 -0500

    a function def one_hot_encode(Y, classes): that converts a numeric label vector into a one-hot matrix

--- supervised_learning/classification/24-one_hot_encode.py
+++ supervised_learning/classification/24-one_hot_encode.py
@@ -2,6 +2,7 @@
 """ Provides converting a numeric label vector into a one-hot matrix."""
 import numpy as np
 
+
 def one_hot_encode(Y, classes):
     """
     Converts a numeric label vector into a one-hot matrix.
