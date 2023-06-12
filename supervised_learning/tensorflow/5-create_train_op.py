bbcda84b272bd5d7f42b1cae1cdf50a767f6b9ee
Your Name <you@example.com>
2023-06-12 10:45:02 -0500

    the function def create_train_op(loss, alpha): that creates the training operation for the network

--- supervised_learning/tensorflow/5-create_train_op.py
+++ supervised_learning/tensorflow/5-create_train_op.py
@@ -4,24 +4,26 @@ Train_Op
 """
 import tensorflow as tf
 
+
+
 def create_train_op(loss, alpha):
     """
     Function that creates the training operation for the network.
 
     Args:
         loss: The loss value of the network's prediction.
-        alpha: The learning rate or step size for 
+        alpha: The learning rate or step size for
         the gradient descent optimizer.
 
     Returns:
         The training operation that minimizes the loss.
     """
-    
+
     # A TensorFlow gradient descent optimizer with the specified learning rate
     optimizer = tf.train.GradientDescentOptimizer(alpha)
-    
+
     # Create the training operation that minimizes the loss
     train_op = optimizer.minimize(loss)
-    
+
     # Return the training operation
     return train_op
