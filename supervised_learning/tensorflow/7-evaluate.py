0f586021243a13e97aafbfb17d045e9fe6b7ef2f
Dennis Agbemenu <105249635+dagbeme1@users.noreply.github.com>
2023-06-12 11:15:36 -0500

    the function def evaluate(X, Y, save_path): that evaluates the output of a neural network:
    X is a numpy.ndarray containing the input data to evaluate
    Y is a numpy.ndarray containing the one-hot labels for X
    save_path is the location to load the model from
    Import the meta graph
    Get the following from the graph’s collection:
    tensors y_pred, loss, and accuracy
    You are not allowed to use tf.saved_model
    Returns: the network’s prediction, accuracy, and loss, respectively

---
+++ supervised_learning/tensorflow/7-evaluate.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+Evaluate the output of a neural network
+"""
+import tensorflow as tf
+
+def evaluate(X, Y, save_path):
+    """function that evaluates the output of a nn"""
+    with tf.Session() as sess:
+        loader = tf.train.import_meta_graph(save_path + '.meta')
+        loader.restore(sess, save_path)
+
+        # Retrieve the required tensors from the graph's collection
+        var_names = ['x', 'y', 'y_pred', 'accuracy', 'loss']
+        for var_name in var_names:
+            globals()[var_name] = tf.get_collection(var_name)[0]
+
+        # Run the evaluation operations
+        y_pred = sess.run(globals()['y_pred'], feed_dict={x: X, y: Y})
+        loss = sess.run(globals()['loss'], feed_dict={x: X, y: Y})
+        acc = sess.run(globals()['accuracy'], feed_dict={x: X, y: Y})
+
+    return y_pred, acc, loss
