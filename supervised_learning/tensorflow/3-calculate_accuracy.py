f1dba6708053e3e09b14a54bd4e748f57f13b798
Your Name <you@example.com>
2023-06-11 22:07:28 -0500

    fixing white spaces, should now work with no issues

--- supervised_learning/tensorflow/3-calculate_accuracy.py
+++ supervised_learning/tensorflow/3-calculate_accuracy.py
@@ -7,13 +7,13 @@ import tensorflow as tf
 
 def calculate_accuracy(y, y_pred):
     """Function that calculates the accuracy of a prediction"""
-    
+
     # Convert true labels and predicted labels to respective class indices
     label = tf.argmax(y, axis=1)
     pred = tf.argmax(y_pred, axis=1)
-    
+
     # From tf .cast change boolean values to float (1 for True, 0 for False)
     # Calculate the mean accuracy over all predictions
     accuracy = tf.reduce_mean(tf.cast(tf.equal(pred, label), tf.float32))
-    
+
     return accuracy
