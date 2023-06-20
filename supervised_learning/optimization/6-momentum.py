b5cd4385d0fc77f318867bf9f740a653c76c5a1e
Your Name <you@example.com>
2023-06-20 18:54:50 -0500

    the function def create_momentum_op(loss, alpha, beta1): that creates the training operation for a neural network in tensorflow using the gradient descent with momentum optimization algorithm

--- supervised_learning/optimization/6-momentum.py
+++ supervised_learning/optimization/6-momentum.py
@@ -8,8 +8,8 @@ import tensorflow as tf
 def create_momentum_op(loss, alpha, beta1):
     """function that implements momentum gradient descent in tensorflow"""
     return tf.train.MomentumOptimizer(
-    # Set the learning rate, momentum, and locking parameters
+        # Set the learning rate, momentum, and locking parameters
         learning_rate=alpha, momentum=beta1, use_locking=False,
-          # Specify the optimizer name and whether to use Nesterov momentum
+        # Specify the optimizer name and whether to use Nesterov momentum
         name='Momentum', use_nesterov=False
     ).minimize(loss)  # Minimize the provided loss using Momentum optimizer
