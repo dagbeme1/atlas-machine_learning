61f683ba68eeb7d2ea6d2c0b8759b1fe399b4a10
Dennis Agbemenu <105249635+dagbeme1@users.noreply.github.com>
2023-06-20 18:53:12 -0500

    the function def create_momentum_op(loss, alpha, beta1): that creates the training operation for a neural network in tensorflow using the gradient descent with momentum optimization algorithm

---
+++ supervised_learning/optimization/6-momentum.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Momentum Upgraded
+"""
+import tensorflow as tf
+
+
+def create_momentum_op(loss, alpha, beta1):
+    """function that implements momentum gradient descent in tensorflow"""
+    return tf.train.MomentumOptimizer(
+    # Set the learning rate, momentum, and locking parameters
+        learning_rate=alpha, momentum=beta1, use_locking=False,
+          # Specify the optimizer name and whether to use Nesterov momentum
+        name='Momentum', use_nesterov=False
+    ).minimize(loss)  # Minimize the provided loss using Momentum optimizer
