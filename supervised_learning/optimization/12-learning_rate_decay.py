b1a2e26cd7be18b1bdf2e194cdf89619c91165a9
Dennis Agbemenu <105249635+dagbeme1@users.noreply.github.com>
2023-06-23 17:31:22 -0500

    the function def learning_rate_decay(alpha, decay_rate, global_step, decay_step): that creates a learning rate decay operation in tensorflow using inverse time decay

--- supervised_learning/optimization/12-learning_rate_decay.py
+++ supervised_learning/optimization/12-learning_rate_decay.py
@@ -10,7 +10,8 @@ def learning_rate_decay(alpha, decay_rate, global_step, decay_step):
 
     # Create a learning rate decay operation using inverse time decay
     # The learning rate will decay over time based on the specified parameters
-    # The decay occurs in a stepwise fashion if the "staircase" parameter is set to True
+    # The decay occurs in a stepwise fashion if
+    # the "staircase" parameter is set to True
     # The operation name is set to None by default
     learning_rate = tf.train.inverse_time_decay(
         learning_rate=alpha, global_step=global_step, decay_steps=decay_step,
