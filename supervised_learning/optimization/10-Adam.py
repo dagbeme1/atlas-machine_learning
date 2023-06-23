0fc2afe927e6ce598f8c458c59641c24ce24690e
Dennis Agbemenu <105249635+dagbeme1@users.noreply.github.com>
2023-06-23 16:15:58 -0500

    the function def create_Adam_op(loss, alpha, beta1, beta2, epsilon): that creates the training operation for a neural network in tensorflow using the Adam optimization algorithm:
    loss is the loss of the network
    alpha is the learning rate
    beta1 is the weight used for the first moment
    beta2 is the weight used for the second moment
    epsilon is a small number to avoid division by zero
    Returns: the Adam optimization operation

---
+++ supervised_learning/optimization/10-Adam.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""
+Adam Upgraded
+"""
+import tensorflow as tf
+
+
+def create_Adam_op(loss, alpha, beta1, beta2, epsilon):
+    """
+    Function that implements Adam gradient descent in TensorFlow.
+    
+    Args:
+        loss: The loss of the network.
+        alpha: The learning rate, which controls the step size during optimization.
+        beta1: The weight used for the first moment estimation in Adam.
+        beta2: The weight used for the second moment estimation in Adam.
+        epsilon: A small number to avoid division by zero.
+        
+    Returns:
+        The Adam optimization operation.
+    """
+    return tf.train.AdamOptimizer(
+        learning_rate=alpha, beta1=beta1, beta2=beta2, epsilon=epsilon,
+        use_locking=False, name='Adam'
+    ).minimize(loss)
