5eddd5dfaeeb56e2cd14e6e37e92cc448349c036
Your Name <you@example.com>
2023-06-23 17:30:35 -0500

    the function def learning_rate_decay(alpha, decay_rate, global_step, decay_step): that creates a learning rate decay operation in tensorflow using inverse time decay

--- supervised_learning/optimization/12-learning_rate_decay.py
+++ supervised_learning/optimization/12-learning_rate_decay.py
@@ -4,9 +4,10 @@ Learning Rate Decay Upgraded
 """
 import tensorflow as tf
 
+
 def learning_rate_decay(alpha, decay_rate, global_step, decay_step):
     """Function that updates the learning rate using inverse time decay"""
-    
+
     # Create a learning rate decay operation using inverse time decay
     # The learning rate will decay over time based on the specified parameters
     # The decay occurs in a stepwise fashion if the "staircase" parameter is set to True
@@ -15,6 +16,6 @@ def learning_rate_decay(alpha, decay_rate, global_step, decay_step):
         learning_rate=alpha, global_step=global_step, decay_steps=decay_step,
         decay_rate=decay_rate, staircase=True, name=None
     )
-    
+
     # Return the learning rate decay operation
     return learning_rate
