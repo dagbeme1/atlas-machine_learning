1f19fd511dbc52adcbd1cd4373ea7f16bea5f946
Your Name <you@example.com>
2023-06-26 16:23:14 -0500

    the function def sensitivity(confusion): that calculates the sensitivity for each class in a confusion matrix

--- supervised_learning/error_analysis/1-sensitivity.py
+++ supervised_learning/error_analysis/1-sensitivity.py
@@ -12,7 +12,7 @@ def sensitivity(confusion):
     Args:
         confusion (numpy.ndarray):
         Confusion matrix of shape (classes, classes),
-            where row indices represent the correct labels and 
+            where row indices represent the correct labels and
             column indices represent the predicted labels.
 
     Returns:
@@ -22,15 +22,15 @@ def sensitivity(confusion):
     classes, classes = confusion.shape  # Get the number of classes
     # Initialize array to store sensitivity values
     sensitivity = np.zeros(shape=(classes,))
-    
+
     for i in range(classes):
         # Calculate true positives for the current class
         true_positives = confusion[i][i]
-        
+
         # Calculate actual positives for the current class
         actual_positives = np.sum(confusion, axis=1)[i]
-        
+
         # Calculate sensitivity for the current class
         sensitivity[i] = true_positives / actual_positives
-    
+
     return sensitivity
