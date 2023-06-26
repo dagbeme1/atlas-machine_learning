c6e93b8ea606822c61df5880e7783e80301bdb04
Your Name <you@example.com>
2023-06-26 16:35:08 -0500

    the function def precision(confusion): that calculates the precision for each class in a confusion matrix

--- supervised_learning/error_analysis/2-precision.py
+++ supervised_learning/error_analysis/2-precision.py
@@ -4,15 +4,16 @@ Precision
 """
 import numpy as np
 
+
 def precision(confusion):
     """Function that calculates the precision for each class."""
-    
+
     # Get the number of classes from the shape of the confusion matrix
     classes, classes = confusion.shape
-    
+
     # Create an array of zeros to store the precision for each class
     precision = np.zeros(shape=(classes,))
-    
+
     # Iterate over each class
     for i in range(classes):
         # Calculate the precision for the current class
@@ -20,6 +21,6 @@ def precision(confusion):
         # by the sum of true positives and false positives
         # for that class (np.sum(confusion, axis=0)[i])
         precision[i] = confusion[i][i] / np.sum(confusion, axis=0)[i]
-    
+
     # Return the array of precision values
     return precision
