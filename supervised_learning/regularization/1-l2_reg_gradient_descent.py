e470fac2c1745c8e0a3a179acf75b501b03e3650
Dennis Agbemenu <105249635+dagbeme1@users.noreply.github.com>
2023-07-04 16:21:51 -0500

    a function def l2_reg_gradient_descent(Y, weights, cache, alpha, lambtha, L): that updates the weights and biases of a neural network using gradient descent with L2 regularization:
    Y is a one-hot numpy.ndarray of shape (classes, m) that contains the correct labels for the data
    classes is the number of classes
    m is the number of data points
    weights is a dictionary of the weights and biases of the neural network
    cache is a dictionary of the outputs of each layer of the neural network
    alpha is the learning rate
    lambtha is the L2 regularization parameter
    L is the number of layers of the network
    The neural network uses tanh activations on each layer except the last, which uses a softmax activation
    The weights and biases of the network should be updated in place

---
+++ supervised_learning/regularization/1-l2_reg_gradient_descent.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""Gradient Descent with L2 Regularization"""
+import numpy as np
+
+def l2_reg_gradient_descent(Y, weights, cache, alpha, lambtha, L):
+    # Create a copy of the weights dictionary
+    weights_copy = weights.copy()
+
+    # Iterate over the layers of the neural network in reverse order
+    for i in range(L, 0, -1):
+        # Get the number of training examples
+        m = Y.shape[1]
+
+        # Calculate the derivative of the activation function for the current layer
+        if i != L:
+            # All layers use a tanh activation, except the last layer
+            dZi = np.multiply(np.matmul(weights_copy['W' + str(i + 1)].T, dZi), 1 - cache['A' + str(i)] ** 2)
+        else:
+            # Last layer uses a softmax activation
+            dZi = cache['A' + str(i)] - Y
+
+        # Calculate the gradients of the weights and biases
+        dWi = np.matmul(dZi, cache['A' + str(i - 1)].T) / m
+        dbi = np.sum(dZi, axis=1, keepdims=True) / m
+
+        # Apply L2 regularization to the weights
+        l2 = (1 - alpha * lambtha / m)
+        weights['W' + str(i)] = l2 * weights_copy['W' + str(i)] - alpha * dWi
+        weights['b' + str(i)] = weights_copy['b' + str(i)] - alpha * dbi
