7d8cd0450d3e83ba50372f25907428e10a8c9c83
Your Name <you@example.com>
2023-07-04 18:35:44 -0500

    a function def dropout_forward_prop(X, weights, L, keep_prob): that conducts forward propagation using Dropout:
    X is a numpy.ndarray of shape (nx, m) containing the input data for the network
    nx is the number of input features
    m is the number of data points
    weights is a dictionary of the weights and biases of the neural network
    L the number of layers in the network
    keep_prob is the probability that a node will be kept
    All layers except the last should use the tanh activation function
    The last layer should use the softmax activation function

--- supervised_learning/regularization/4-dropout_forward_prop.py
+++ supervised_learning/regularization/4-dropout_forward_prop.py
@@ -6,6 +6,7 @@ Forward Propagation with Dropout
 
 import numpy as np
 
+
 def dropout_forward_prop(X, weights, L, keep_prob):
     """
     Function that conducts forward propagation using Dropout.
@@ -29,29 +30,29 @@ def dropout_forward_prop(X, weights, L, keep_prob):
     # Perform forward propagation for each layer
     for i in range(L):
         # Compute the linear transformation of the input
-        Zi = np.matmul(weights['W' + str(i + 1)], 
+        Zi = np.matmul(weights['W' + str(i + 1)],
                        cache['A' + str(i)]) + weights['b' + str(i + 1)]
 
         # Apply the appropriate activation function based on the layer index
         if i == L - 1:
             # For the last layer, apply the softmax activation function
-            cache['A' + str(i + 1)] = 
+            cache['A' + str(i + 1)] =
             np.exp(Zi) / np.sum(np.exp(Zi), axis=0, keepdims=True)
         else:
             # For other layers, apply
             # the hyperbolic tangent (tanh) activation function
             cache['A' + str(i + 1)] = np.tanh(Zi)
 
-            # Apply dropout by randomly setting 
+            # Apply dropout by randomly setting
             # a fraction of the values in the layer to zero
             boolean = np.random.rand
-            (cache['A' + str(i + 1)].shape[0], 
+            (cache['A' + str(i + 1)].shape[0],
              cache['A' + str(i + 1)].shape[1]) < keep_prob
             drop = np.where(boolean == 1, 1, 0)
             cache['A' + str(i + 1)] *= drop
             cache['A' + str(i + 1)] /= keep_prob
 
-            # Store the dropout mask in 
+            # Store the dropout mask in
             # the cache for later use in backpropagation
             cache['D' + str(i + 1)] = drop
 
