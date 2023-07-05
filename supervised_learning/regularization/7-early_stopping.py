738fd7ad74f48472148d2580988f55f6c1ab821b
Dennis Agbemenu <105249635+dagbeme1@users.noreply.github.com>
2023-07-04 23:47:21 -0500

    the function def early_stopping(cost, opt_cost, threshold, patience, count): that determines if you should stop gradient descent early:
    Early stopping should occur when the validation cost of the network has not decreased relative to the optimal validation cost by more than the threshold over a specific patience count
    cost is the current validation cost of the neural network
    opt_cost is the lowest recorded validation cost of the neural network
    threshold is the threshold used for early stopping
    patience is the patience count used for early stopping
    count is the count of how long the threshold has not been met
    Returns: a boolean of whether the network should be stopped early, followed by the updated count

---
+++ supervised_learning/regularization/7-early_stopping.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+
+"""
+Early Stopping
+"""
+
+def early_stopping(cost, opt_cost, threshold, patience, count):
+    """
+    Determines if gradient descent should be stopped early based on validation cost.
+
+    Arguments:
+    - cost: Current validation cost of the neural network.
+    - opt_cost: Lowest recorded validation cost of the neural network.
+    - threshold: Threshold used for early stopping.
+    - patience: Patience count used for early stopping.
+    - count: Count of how long the threshold has not been met.
+
+    Returns:
+    - Tuple containing a boolean indicating
+    whether to stop early and updated count value.
+    """
+
+    # Initialize early stopping flag as False
+    early_stopping = False
+
+    # Calculate the difference between optimal cost and current cost
+    cost_difference = opt_cost - cost
+
+    # Check if the cost difference is less than or equal to the threshold
+    if cost_difference <= threshold:
+        # Increment the count since the threshold has not been met
+        count += 1
+    else:
+        # Reset the count since the threshold has been met
+        count = 0
+
+    # Check if the count has reached the patience limit
+    if count == patience:
+        # Early stopping condition met, set early stopping flag to True
+        early_stopping = True
+        return (early_stopping, count)
+
+    # Return the early stopping flag and the updated count value
+    return (early_stopping, count)
