3b22dd6a47fb84d1e603745b503e9a7d5d8070f2
Your Name <you@example.com>
2023-07-05 02:03:55 -0500

    a function def optimize_model(network, alpha, beta1, beta2): that sets up Adam optimization for a keras model with categorical crossentropy loss and accuracy metrics

--- supervised_learning/keras/2-optimize.py
+++ supervised_learning/keras/2-optimize.py
@@ -10,12 +10,12 @@ def optimize_model(network, alpha, beta1, beta2):
     """function that sets up an Adam optimizer"""
     # Create an Adam optimizer with specified learning rate, beta1, beta2
     optimizer = K.optimizers.Adam(lr=alpha, beta_1=beta1, beta_2=beta2)
-    
+
     # Define the loss function as categorical cross-entropy
     loss = 'categorical_crossentropy'
-    
+
     # Compile the network with optimizer, loss function, and accuracy metric
     network.compile(optimizer=optimizer, loss=loss, metrics=['accuracy'])
-    
+
     # Return None
     return None
