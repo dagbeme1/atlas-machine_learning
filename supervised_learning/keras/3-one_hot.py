eff751b13cff20f4e8c3e80f425240f13f3bdeae
Your Name <you@example.com>
2023-07-05 02:22:16 -0500

    a function def one_hot(labels, classes=None): that converts a label vector into a one-hot matrix

--- supervised_learning/keras/3-one_hot.py
+++ supervised_learning/keras/3-one_hot.py
@@ -8,5 +8,6 @@ import tensorflow.keras as K
 
 def one_hot(labels, classes=None):
     """function that converts a label vector into a one-hot matrix"""
-    # Use the to_categorical function from Keras to convert labels into one-hot encoding
+    # Use the to_categorical function from Keras to convert labels into
+    # one-hot encoding
     return K.utils.to_categorical(labels, num_classes=classes)
