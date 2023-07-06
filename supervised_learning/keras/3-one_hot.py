fc1ef29d8ae8bebbb738749993cc607801f258c9
Dennis Agbemenu <105249635+dagbeme1@users.noreply.github.com>
2023-07-05 22:33:36 -0500

    a function def one_hot(labels, classes=None): that converts a label vector into a one-hot matrix:
    The last dimension of the one-hot matrix must be the number of classes
    Returns: the one-hot matrix

---
+++ supervised_learning/keras/3-one_hot.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+"""
+One Hot
+"""
+import tensorflow.keras as K
+
+
+def one_hot(labels, classes=None):
+    """function that converts a label vector into a one-hot matrix"""
+    # Use the to_categorical function from Keras to convert labels into one-hot encoding
+    return K.utils.to_categorical(labels, num_classes=classes)
