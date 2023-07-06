fc7163d58d2d01d8287a5d7820ce5d37696198fd
Dennis Agbemenu <105249635+dagbeme1@users.noreply.github.com>
2023-07-06 01:55:07 -0500

    a function def convolve_grayscale_padding(images, kernel, padding): that performs a convolution on grayscale images with custom padding:
    images is a numpy.ndarray with shape (m, h, w) containing multiple grayscale images
    m is the number of images
    h is the height in pixels of the images
    w is the width in pixels of the images
    kernel is a numpy.ndarray with shape (kh, kw) containing the kernel for the convolution
    kh is the height of the kernel
    kw is the width of the kernel
    padding is a tuple of (ph, pw)
    ph is the padding for the height of the image
    pw is the padding for the width of the image
    the image should be padded with 0’s
    You are only allowed to use two for loops; any other loops of any kind are not allowed
    Returns: a numpy.ndarray containing the convolved images

---
+++ math/convolutions_and_pooling/2-convolve_grayscale_padding.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+"""
+Convolution with Padding
+"""
+
+import numpy as np
+
+
+def convolve_grayscale_padding(images, kernel, padding):
+    """function that performs a convolution with custom padding"""
+    m = images.shape[0]  # Number of images
+    h = images.shape[1]  # Height of images
+    w = images.shape[2]  # Width of images
+    kh = kernel.shape[0]  # Height of kernel
+    kw = kernel.shape[1]  # Width of kernel
+    image_num = np.arange(m)  # Array of image indices
+    ph = padding[0]  # Padding height
+    pw = padding[1]  # Padding width
+
+    # Pad images before convolution, padding always symmetric here
+    padded_images = np.pad(images, pad_width=((0, 0), (ph, ph), (pw, pw)),
+                           mode='constant')
+    
+    # Output size depends on filter size and padding
+    output = np.zeros(shape=(m, h - kh + 1 + 2 * ph, w - kw + 1 + 2 * pw))
+
+    for i in range(h - kh + 1 + 2 * ph):
+        for j in range(w - kw + 1 + 2 * pw):
+            # Perform element-wise multiplication of
+            # the padded image patch and kernel,
+            # then sum the results along
+            # the height and width dimensions (axis 1 and axis 2)
+            output[
+                image_num,
+                i,
+                j
+            ] = np.sum(
+                padded_images[
+                    image_num,
+                    i: i + kh,
+                    j: j + kw
+                ] * kernel,
+                axis=(1, 2)
+            )
+    return output
