e8918a965eb34b9e6d61def15f686692a8fe8f66
Dennis Agbemenu <105249635+dagbeme1@users.noreply.github.com>
2023-07-10 17:05:56 -0500

    a function def lenet5(x, y): that builds a modified version of the LeNet-5 architecture using tensorflow:
    x is a tf.placeholder of shape (m, 28, 28, 1) containing the input images for the network
    m is the number of images
    y is a tf.placeholder of shape (m, 10) containing the one-hot labels for the network
    The model should consist of the following layers in order:
    Convolutional layer with 6 kernels of shape 5x5 with same padding
    Max pooling layer with kernels of shape 2x2 with 2x2 strides
    Convolutional layer with 16 kernels of shape 5x5 with valid padding
    Max pooling layer with kernels of shape 2x2 with 2x2 strides
    Fully connected layer with 120 nodes
    Fully connected layer with 84 nodes
    Fully connected softmax output layer with 10 nodes
    All layers requiring initialization should initialize their kernels with the he_normal initialization method: tf.contrib.layers.variance_scaling_initializer()
    All hidden layers requiring activation should use the relu activation function
    you may import tensorflow as tf
    you may NOT use tf.keras
    Returns:
    a tensor for the softmax activated output
    a training operation that utilizes Adam optimization (with default hyperparameters)
    a tensor for the loss of the netowrk
    a tensor for the accuracy of the network

---
+++ supervised_learning/cnn/4-lenet5.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python3
+
+"""
+LeNet-5 in Tensorflow
+"""
+# Description of the purpose of the code
+
+import tensorflow as tf  # Importing the tensorflow module for deep learning
+
+def lenet5(x, y):
+    """function that builds a modified version of LeNet-5"""
+    # Function definition with input parameters and docstring
+
+    # Initializer for the layer weights
+    initializer = tf.contrib.layers.variance_scaling_initializer()
+    layer = tf.layers.Conv2D(filters=6,
+                             kernel_size=5,
+                             padding='same',
+                             kernel_initializer=initializer,
+                             activation=tf.nn.relu)  # Convolutional layer with 6 filters,
+    # kernel size 5x5, same padding, ReLU activation
+    output = layer(x)  # Apply the convolutional layer to the input
+    layer = tf.layers.MaxPooling2D(pool_size=2,
+                                   strides=2)  # Max pooling layer
+    # with pool size 2x2 and stride 2
+    output = layer(output)  # Apply the max pooling layer to the output
+    layer = tf.layers.Conv2D(filters=16,
+                             kernel_size=5,
+                             padding='valid',
+                             kernel_initializer=initializer,
+                             # Convolutional layer with 16 filters,
+                             # kernel size 5x5, valid padding, ReLU activation
+                             activation=tf.nn.relu)
+    output = layer(output)  # Apply the convolutional layer to the output
+    layer = tf.layers.MaxPooling2D(pool_size=2,
+                                   # Max pooling layer with
+                                   # pool size 2x2 and stride 2
+                                   strides=2)
+    output = layer(output)  # Apply the max pooling layer to the output
+    layer = tf.layers.Flatten()  # Flatten the output to a 1D tensor
+    output = layer(output)  # Apply the flatten layer to the output
+    layer = tf.layers.Dense(units=120,
+                            activation=tf.nn.relu,
+                            # Fully connected layer with 120 units,
+                            # ReLU activation
+                            kernel_initializer=initializer)
+    output = layer(output)  # Apply the fully connected layer to the output
+    layer = tf.layers.Dense(units=84,
+                            activation=tf.nn.relu,
+                            # Fully connected layer with 84 units,
+                            # ReLU activation
+                            kernel_initializer=initializer)
+    output = layer(output)  # Apply the fully connected layer to the output
+    layer = tf.layers.Dense(units=10,
+                            # Fully connected layer with 10 units
+                            kernel_initializer=initializer)
+    output = layer(output)  # Apply the fully connected layer to the output
+
+    # Define loss as softmax cross-entropy between the
+    # true labels and unactivated output (logits)
+    loss = tf.losses.softmax_cross_entropy(y, output)
+
+    # Define an Adam optimizer with default learning rate & minimize the loss
+    train_op = tf.train.AdamOptimizer().minimize(loss)
+
+    # Activate the output with softmax
+    y_pred = tf.nn.softmax(output)
+
+    # Evaluate the accuracy of the model
+    acc = accuracy(y, y_pred)
+
+    return y_pred, train_op, loss, acc
+
+
+def accuracy(y, y_pred):
+    """evaluate the accuracy of the model"""
+    label = tf.argmax(y, axis=1)  # Get the index of the true label
+    pred = tf.argmax(y_pred, axis=1)  # Get the index of the predicted label
+    # Compute the accuracy as the mean of correct predictions
+    acc = tf.reduce_mean(tf.cast(tf.equal(label, pred), tf.float32))
+    return acc
---
+++ supervised_learning/cnn/4-main.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+
+import matplotlib.pyplot as plt  # Importing the matplotlib.pyplot module for plotting
+import numpy as np  # Importing the numpy module for numerical computations
+import tensorflow as tf  # Importing the tensorflow module for deep learning
+lenet5 = __import__('4-lenet5').lenet5  # Importing the lenet5 function from the specified module
+
+if __name__ == "__main__":
+    # Execute the following code only if the script is run directly and not imported as a module
+
+    np.random.seed(0)  # Setting the random seed for reproducibility
+    tf.set_random_seed(0)  # Setting the random seed for reproducibility in TensorFlow
+
+    # Load the MNIST dataset from the specified file
+    lib = np.load('../data/MNIST.npz')
+    X_train = lib['X_train']  # Get the training images from the loaded dataset
+    Y_train = lib['Y_train']  # Get the training labels from the loaded dataset
+    X_valid = lib['X_valid']  # Get the validation images from the loaded dataset
+    Y_valid = lib['Y_valid']  # Get the validation labels from the loaded dataset
+    m, h, w = X_train.shape  # Get the number of examples, height, and width of the training images
+    X_train_c = X_train.reshape((-1, h, w, 1))  # Reshape the training images to match the expected input shape
+    X_valid_c = X_valid.reshape((-1, h, w, 1))  # Reshape the validation images to match the expected input shape
+
+    x = tf.placeholder(tf.float32, (None, h, w, 1))  # Placeholder for the input images
+    y = tf.placeholder(tf.int32, (None,))  # Placeholder for the target labels
+    y_oh = tf.one_hot(y, 10)  # One-hot encoding of the target labels
+    y_pred, train_op, loss, acc = lenet5(x, y_oh)  # Build the LeNet-5 model and define the training operations and metrics
+
+    batch_size = 32  # Size of each mini-batch
+    epochs = 10  # Number of training epochs
+
+    init = tf.global_variables_initializer()  # Initialize all the variables
+    with tf.Session() as sess:
+        sess.run(init)  # Run the initialization operation
+
+        for epoch in range(epochs):
+            cost, accuracy = sess.run((loss, acc), feed_dict={x: X_train_c, y: Y_train})  # Compute the cost and accuracy on the training set
+            cost_valid, accuracy_valid = sess.run((loss, acc), feed_dict={x: X_valid_c, y: Y_valid})  # Compute the cost and accuracy on the validation set
+            print("After {} epochs: {} cost, {} accuracy, {} validation cost, {} validation accuracy".format(epoch, cost, accuracy, cost_valid, accuracy_valid))
+
+            p = np.random.permutation(m)  # Shuffle the indices of the training examples
+            X_shuffle = X_train_c[p]  # Shuffle the training images
+            Y_shuffle = Y_train[p]  # Shuffle the training labels
+
+            for i in range(0, m, batch_size):
+                X_batch = X_shuffle[i:i+batch_size]  # Get the mini-batch of training images
+                Y_batch = Y_shuffle[i:i+batch_size]  # Get the mini-batch of training labels
+                sess.run(train_op, feed_dict={x: X_batch, y: Y_batch})  # Perform a training step using the mini-batch
+
+        cost, accuracy = sess.run((loss, acc), feed_dict={x: X_train_c, y: Y_train})  # Compute the final cost and accuracy on the training set
+        cost_valid, accuracy_valid = sess.run((loss, acc), feed_dict={x: X_valid_c, y: Y_valid})  # Compute the final cost and accuracy on the validation set
+        print("After {} epochs: {} cost, {} accuracy, {} validation cost, {} validation accuracy".format(epochs, cost, accuracy, cost_valid, accuracy_valid))
+
+        Y_pred = sess.run(y_pred, feed_dict={x: X_valid_c, y: Y_valid})  # Get the predicted labels for the validation set
+        print(Y_pred[0])
+        Y_pred = np.argmax(Y_pred, 1)  # Convert the one-hot encoded predictions to class labels
+        plt.imshow(X_valid[0])  # Display the first validation image
+        plt.title(str(Y_valid[0]) + ' : ' + str(Y_pred[0]))  # Set the title of the plot with the true label and predicted label
+        plt.show()  # Show the plot
