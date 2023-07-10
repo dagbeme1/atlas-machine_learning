78c65fc6210870967e3d6fda8c1ba7a45ff02c26
Dennis Agbemenu <105249635+dagbeme1@users.noreply.github.com>
2023-07-09 23:13:07 -0500

    a function def conv_forward(A_prev, W, b, activation, padding="same", stride=(1, 1)): that performs forward propagation over a convolutional layer of a neural network:
    A_prev is a numpy.ndarray of shape (m, h_prev, w_prev, c_prev) containing the output of the previous layer
    m is the number of examples
    h_prev is the height of the previous layer
    w_prev is the width of the previous layer
    c_prev is the number of channels in the previous layer
    W is a numpy.ndarray of shape (kh, kw, c_prev, c_new) containing the kernels for the convolution
    kh is the filter height
    kw is the filter width
    c_prev is the number of channels in the previous layer
    c_new is the number of channels in the output
    b is a numpy.ndarray of shape (1, 1, 1, c_new) containing the biases applied to the convolution
    activation is an activation function applied to the convolution
    padding is a string that is either same or valid, indicating the type of padding used
    stride is a tuple of (sh, sw) containing the strides for the convolution
    sh is the stride for the height
    sw is the stride for the width
    you may import numpy as np
    Returns: the output of the convolutional layer

---
+++ supervised_learning/cnn/0-main.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+import matplotlib.pyplot as plt  # Importing the matplotlib.pyplot module for plotting
+import numpy as np  # Importing the numpy module for numerical computations
+
+conv_forward = __import__('0-conv_forward').conv_forward  # Importing the conv_forward function from the specified module
+
+if __name__ == "__main__":
+    # Execute the following code only if the script is run directly and not imported as a module
+    
+    np.random.seed(0)  # Setting the random seed for reproducibility
+    
+    # Load the MNIST dataset from the specified file
+    lib = np.load('../data/MNIST.npz')
+    X_train = lib['X_train']  # Get the training images from the loaded dataset
+    m, h, w = X_train.shape  # Get the number of training examples, height, and width of the images
+    X_train_c = X_train.reshape((-1, h, w, 1))  # Reshape the training images to have a single channel
+
+    # Initialize the weights and biases for the convolutional layer
+    W = np.random.randn(3, 3, 1, 2)  # Randomly initialize the filter weights
+    b = np.random.randn(1, 1, 1, 2)  # Randomly initialize the biases
+
+    def relu(Z):
+        return np.maximum(Z, 0)  # ReLU activation function to apply element-wise on an input array
+
+    # Display the first training image using matplotlib
+    plt.imshow(X_train[0])
+    plt.show()
