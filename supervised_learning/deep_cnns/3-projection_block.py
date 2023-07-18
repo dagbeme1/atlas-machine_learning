3e4c70cf821564d59720fc9401da90b5378f1c8b
Your Name <you@example.com>
2023-07-17 20:14:17 -0500

    a function def projection_block(A_prev, filters, s=2): that builds a projection block as described in Deep Residual Learning for Image Recognition (2015)

--- supervised_learning/deep_cnns/3-projection_block.py
+++ supervised_learning/deep_cnns/3-projection_block.py
@@ -6,12 +6,16 @@ import tensorflow.keras as K
 # Import the projection_block function from the '2-projection_block' module
 projection_block = __import__('2-projection_block').projection_block
 
-# Function definition for a projection block, which is a building block of the ResNet architecture
+# Function definition for a projection block, which is a building block of
+# the ResNet architecture
+
+
 def projection_block(A_prev, filters, s=2):
     # Use He normal initializer for the weights
     initializer = K.initializers.he_normal()
 
-    # Layer 1: 1x1 Convolution with 'filters[0]' filters, stride 's', and no activation (linear)
+    # Layer 1: 1x1 Convolution with 'filters[0]' filters, stride 's', and no
+    # activation (linear)
     F11_layer = K.layers.Conv2D(filters=filters[0],
                                 kernel_size=1,
                                 padding='same',
@@ -26,7 +30,8 @@ def projection_block(A_prev, filters, s=2):
     F11_activ = K.layers.Activation('relu')
     F11_output = F11_activ(F11_output)
 
-    # Layer 2: 3x3 Convolution with 'filters[1]' filters and no activation (linear)
+    # Layer 2: 3x3 Convolution with 'filters[1]' filters and no activation
+    # (linear)
     F3_layer = K.layers.Conv2D(filters=filters[1],
                                kernel_size=3,
                                padding='same',
@@ -40,7 +45,8 @@ def projection_block(A_prev, filters, s=2):
     F3_activ = K.layers.Activation('relu')
     F3_output = F3_activ(F3_output)
 
-    # Layer 3: 1x1 Convolution with 'filters[2]' filters and no activation (linear)
+    # Layer 3: 1x1 Convolution with 'filters[2]' filters and no activation
+    # (linear)
     F12_layer = K.layers.Conv2D(filters=filters[2],
                                 kernel_size=1,
                                 padding='same',
@@ -51,7 +57,8 @@ def projection_block(A_prev, filters, s=2):
     F12_norm = K.layers.BatchNormalization()
     F12_output = F12_norm(F12_output)
 
-    # Layer 4: 1x1 Convolution with 'filters[2]' filters, stride 's', and no activation (linear)
+    # Layer 4: 1x1 Convolution with 'filters[2]' filters, stride 's', and no
+    # activation (linear)
     F12_bypass_layer = K.layers.Conv2D(filters=filters[2],
                                        kernel_size=1,
                                        padding='same',
