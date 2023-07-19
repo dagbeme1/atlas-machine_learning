8e0106bb8b68141a0adaa336c4926f77a470be00
Dennis Agbemenu <105249635+dagbeme1@users.noreply.github.com>
2023-07-18 19:28:36 -0500

    a function def inception_network(): that builds the inception network as described in Going Deeper with Convolutions (2014)

--- supervised_learning/deep_cnns/1-inception_network.py
+++ supervised_learning/deep_cnns/1-inception_network.py
@@ -1,49 +1,44 @@
 #!/usr/bin/env python3
+# The shebang line at the beginning tells the system to use the Python 3 interpreter
+# to execute this script.
 
 """DCCN - Inception Network"""
+# A multiline string (docstring) explaining that this script implements a DCCN Inception Network.
 
 import tensorflow.keras as T
 # Importing the TensorFlow Keras module with the alias 'T'.
 
 inception_block_custom = __import__('0-inception_block').inception_block
-# Importing the 'inception_block' function from
-# the '0-inception_block' module using the custom import method.
+# Importing the 'inception_block' function from the '0-inception_block' module using the custom import method.
 
 def inception_network_custom():
     """Inception Network"""
     # A docstring explaining that this function creates an Inception Network.
 
     input_shape_custom = T.Input(shape=(224, 224, 3))
-    # Creating an input tensor with shape
-    # (224, 224, 3) using TensorFlow Keras Input.
+    # Creating an input tensor with shape (224, 224, 3) using TensorFlow Keras Input.
 
     X = T.layers.Conv2D(64, (7, 7), strides=(2, 2),
                         padding='same', activation='relu')(input_shape_custom)
-    # Adding a 2D convolutional layer with
-    # 64 filters, a kernel size of (7, 7),
-    # a stride of (2, 2), 'same' padding,
-    # and ReLU activation function to the input tensor.
+    # Adding a 2D convolutional layer with 64 filters, a kernel size of (7, 7),
+    # a stride of (2, 2), 'same' padding, and ReLU activation function to the input tensor.
 
     X = T.layers.MaxPool2D((3, 3), strides=(2, 2), padding='same')(X)
     # Adding a 2D max-pooling layer with pool size (3, 3), stride (2, 2),
     # 'same' padding to the previous convolutional layer.
 
     X = T.layers.Conv2D(192, (3, 3), activation='relu', padding='same')(X)
-    # Adding another 2D convolutional layer
-    # with 192 filters, a kernel size of (3, 3),
+    # Adding another 2D convolutional layer with 192 filters, a kernel size of (3, 3),
     # ReLU activation function, and 'same' padding.
 
     X = T.layers.MaxPool2D((3, 3), strides=(2, 2), padding='same')(X)
-    # Adding another 2D max-pooling layer
-    # with pool size (3, 3), stride (2, 2),
+    # Adding another 2D max-pooling layer with pool size (3, 3), stride (2, 2),
     # and 'same' padding to the previous convolutional layer.
 
     X = inception_block_custom(X, [64, 96, 128, 16, 32, 32])
-    # Adding a custom inception block with the provided
-    # filter configuration to the previous layer.
+    # Adding a custom inception block with the provided filter configuration to the previous layer.
 
-    # Several more custom inception blocks are
-    # added with different filter configurations.
+    # Several more custom inception blocks are added with different filter configurations.
 
     X = T.layers.AveragePooling2D(pool_size=(7, 7),
                                   strides=(7, 7),
@@ -52,16 +47,14 @@ def inception_network_custom():
     # and 'valid' padding to the previous inception blocks.
 
     X = T.layers.Dropout(0.4)(X)
-    # Adding a dropout layer with a dropout rate of 0.4 to the previous layer
+    # Adding a dropout layer with a dropout rate of 0.4 to the previous layer.
 
     X = T.layers.Dense(1000, activation='softmax')(X)
-    # Adding a dense (fully connected) layer
-    # with 1000 units and softmax activation
+    # Adding a dense (fully connected) layer with 1000 units and softmax activation
     # to the previous dropout layer.
 
     model_custom = T.models.Model(inputs=input_shape_custom, outputs=X)
-    # Creating a TensorFlow Keras Model with input_shape_custom
-    # as the input and X as the output.
+    # Creating a TensorFlow Keras Model with input_shape_custom as the input and X as the output.
 
     return model_custom
     # Returning the created TensorFlow Keras model from the function.
