9e10a31333839fac829b178e7f5e328e36da3743
Dennis Agbemenu <105249635+dagbeme1@users.noreply.github.com>
2023-07-18 19:13:17 -0500

    a function def inception_network(): that builds the inception network as described in Going Deeper with Convolutions (2014)

--- supervised_learning/deep_cnns/1-inception_network.py
+++ supervised_learning/deep_cnns/1-inception_network.py
@@ -2,18 +2,13 @@
 
 """CustomInception - Neural Network"""
 
-import tensorflow.keras as K
-# Importing the Keras module from TensorFlow with the alias 'K'.
-
 inception_block = __import__('0-inception_block').inception_block
 # Importing the 'inception_block' function from the '0-inception_block' module
 
-from custom_inception_blocks import my_inception_block
-# Importing the custom_inception_block function
-# from the custom_inception_blocks module.
-
 def create_custom_inception_network():
     """Custom Inception Network"""
+    # A docstring explaining that this function
+    # creates a Custom Inception Network.
 
     input_shape_custom = K.Input(shape=(224, 224, 3))
     # Creating an input tensor with shape (224, 224, 3) using Keras Input.
@@ -22,29 +17,29 @@ def create_custom_inception_network():
                         padding='same', activation='relu')(input_shape_custom)
     # Adding a 2D convolutional layer with 64 filters,
     # a kernel size of (7, 7),
-    # a stride of (2, 2), 'same' padding, and
-    # ReLU activation function to the input tensor.
+    # a stride of (2, 2), 'same' padding,
+    # & ReLU activation function to the input tensor.
 
     X = K.layers.MaxPool2D((3, 3), strides=(2, 2), padding='same')(X)
     # Adding a 2D max-pooling layer with pool size (3, 3), stride (2, 2),
     # 'same' padding to the previous convolutional layer.
 
     X = K.layers.Conv2D(192, (3, 3), activation='relu', padding='same')(X)
-    # Adding another 2D convolutional layer
-    # with 192 filters, a kernel size of (3, 3),
+    # Adding another 2D convolutional layer with
+    # 192 filters, a kernel size of (3, 3),
     # ReLU activation function, and 'same' padding.
 
     X = K.layers.MaxPool2D((3, 3), strides=(2, 2), padding='same')(X)
-    # Adding another 2D max-pooling layer
-    # with pool size (3, 3), stride (2, 2),
+    # Adding another 2D max-pooling layer with
+    # pool size (3, 3), stride (2, 2),
     # and 'same' padding to the previous convolutional layer.
 
-    X = my_inception_block(X, [64, 96, 128, 16, 32, 32])
+    X = inception_block(X, [64, 96, 128, 16, 32, 32])
     # Adding a custom inception block with the provided
     # filter configuration to the previous layer.
 
-    # Several more custom inception blocks are added
-    # with different filter configurations.
+    # Several more custom inception blocks are
+    # added with different filter configurations.
 
     X = K.layers.AveragePooling2D(pool_size=(7, 7),
                                   strides=(7, 7),
@@ -53,7 +48,7 @@ def create_custom_inception_network():
     # and 'valid' padding to the previous inception blocks.
 
     X = K.layers.Dropout(0.4)(X)
-    # Adding a dropout layer with a dropout rate of 0.4 to the previous layer.
+    # Adding a dropout layer with a dropout rate of 0.4 to the previous layer
 
     X = K.layers.Dense(1000, activation='softmax')(X)
     # Adding a dense (fully connected) layer with
