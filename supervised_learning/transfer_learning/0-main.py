9f1a5842c9bd96c11fbddec5252de866ae458a5a
Dennis Agbemenu <105249635+dagbeme1@users.noreply.github.com>
2023-07-25 19:12:20 -0500

    a python script that trains a convolutional neural network to classify the CIFAR 10 dataset:
    You must use one of the applications listed in Keras Applications
    Your script must save your trained model in the current working directory as cifar10.h5
    Your saved model should be compiled
    Your saved model should have a validation accuracy of 87% or higher
    Your script should not run when the file is imported
    Hint1: The training and tweaking of hyperparameters may take a while so start early!
    Hint2: The CIFAR 10 dataset contains 32x32 pixel images, however most of the Keras applications are trained on much larger images. Your first layer should be a lambda layer that scales up the data to the correct size
    Hint3: You will want to freeze most of the application layers. Since these layers will always produce the same output, you should compute the output of the frozen layers ONCE and use those values as input to train the remaining trainable layers. This will save you A LOT of time.
    In the same file, write a function def preprocess_data(X, Y): that pre-processes the data for your model:
    
    X is a numpy.ndarray of shape (m, 32, 32, 3) containing the CIFAR 10 data, where m is the number of data points
    Y is a numpy.ndarray of shape (m,) containing the CIFAR 10 labels for X
    Returns: X_p, Y_p
    X_p is a numpy.ndarray containing the preprocessed X
    Y_p is a numpy.ndarray containing the preprocessed Y

--- supervised_learning/transfer_learning/0-main.py
+++ supervised_learning/transfer_learning/0-main.py
@@ -1,22 +1,9 @@
 #!/usr/bin/env python3
-
 import tensorflow.keras as K
-
 preprocess_data = __import__('0-transfer').preprocess_data
-# Importing the preprocess_data function from the '0-transfer' module. This function is used for preprocessing the CIFAR-10 data.
 
-# to fix issue with saving keras applications
 K.learning_phase = K.backend.learning_phase
-# Fixing an issue with saving Keras applications by setting the learning phase of Keras backend.
-
 _, (X, Y) = K.datasets.cifar10.load_data()
-# Loading the CIFAR-10 dataset and unpacking it into X (images) and Y (labels).
-
 X_p, Y_p = preprocess_data(X, Y)
-# Preprocessing the CIFAR-10 data using the preprocess_data function. This function will preprocess the data and one-hot encode the labels.
-
 model = K.models.load_model('cifar10.h5')
-# Loading the pre-trained model stored in 'cifar10.h5' from the current working directory.
-
 model.evaluate(X_p, Y_p, batch_size=128, verbose=1)
-# Evaluating the pre-trained model on the preprocessed test data X_p and Y_p. The batch_size is set to 128, and the evaluation results (loss and accuracy) are printed with verbosity level 1.
