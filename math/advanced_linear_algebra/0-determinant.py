9bd2b06a9e87060ea4c397778022d5569b744e51
Your Name <you@example.com>
2023-09-12 00:44:49 -0500

    fixing a function def determinant(matrix): that calculates the determinant of a matrix

--- math/advanced_linear_algebra/0-determinant.py
+++ math/advanced_linear_algebra/0-determinant.py
@@ -3,10 +3,9 @@
 Advanced Linear Algebra Determinant
 """
 
-
 def determinant(matrix):
     """
-    Calculates the determinant of a square matrix.
+    Calculates the determinant of a matrix.
 
     Args:
         matrix (list): matrix to calculate.
@@ -26,33 +25,34 @@ def determinant(matrix):
     num_rows = len(matrix)
     num_cols = len(matrix[0])
 
-    # Check if the matrix is square
-    if num_rows != num_cols:
-        raise ValueError("matrix must be a square matrix")
-
     # Base case: 0x0 matrix
     if num_rows == 0 and num_cols == 0:
         return 1  # 0x0 matrix, return 1 by convention
 
     # Base case: 1x1 matrix
-    if num_rows == 1:
+    if num_rows == 1 and num_cols == 1:
         return matrix[0][0]
 
     # Initialize the determinant
     det = 0
 
     # Choose the method based on the size of the matrix
-    if num_rows == 2:
-        # Handle base case for 2x2 matrix
-        return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
+    if num_rows == num_cols:
+        # Square matrix
+        if num_rows == 2:
+            # Handle base case for 2x2 square matrix
+            return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
+        else:
+            # Iterate through the column indices
+            for col in range(num_cols):
+                # Create a submatrix without the first row and the current column
+                submatrix = [row[0:col] + row[col+1:] for row in matrix[1:]]
+                # Calculate the cofactor
+                cofactor = matrix[0][col] * determinant(submatrix)
+                # Add or subtract the cofactor to the determinant
+                det += cofactor if col % 2 == 0 else -cofactor
     else:
-        # Iterate through the column indices
-        for col in range(num_cols):
-            # Create a submatrix without the first row and the current column
-            submatrix = [row[0:col] + row[col + 1:] for row in matrix[1:]]
-            # Calculate the cofactor
-            cofactor = matrix[0][col] * determinant(submatrix)
-            # Add or subtract the cofactor to the determinant
-            det += cofactor if col % 2 == 0 else -cofactor
+        # Non-square matrix
+        raise ValueError("matrix must be a square matrix")
 
     return det
