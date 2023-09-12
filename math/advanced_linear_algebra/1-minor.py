06e4fefea746a93842d070c729d495af20c329e4
Your Name <you@example.com>
2023-09-12 01:56:47 -0500

    try again fixing matrix is not square or is empty(take 5)

--- math/advanced_linear_algebra/1-minor.py
+++ math/advanced_linear_algebra/1-minor.py
@@ -3,6 +3,7 @@
 Advanced Linear Algebra (Determinant and Minor)
 """
 
+
 def minor(matrix):
     """
     Calculates the minor matrix of a square matrix.
@@ -14,15 +15,20 @@ def minor(matrix):
         list of lists: The minor matrix.
     """
     # Check if matrix is a list of lists
-    if not isinstance(matrix, list) or not all(isinstance(row, list) for row in matrix):
+    if not isinstance(
+        matrix,
+        list) or not all(
+        isinstance(
+            row,
+            list) for row in matrix):
         raise TypeError("matrix must be a list of lists")
 
     # Get the size (number of rows) of the matrix
     n = len(matrix)
 
-    # Check if the matrix is square
+    # Check if the matrix is square and non-empty
     if n == 0 or len(matrix[0]) != n:
-        raise ValueError("matrix must be a square matrix")  # Update the error message
+        raise ValueError("matrix must be a non-empty square matrix")
 
     # Handle the case of a 1x1 matrix
     if n == 1:
@@ -38,7 +44,8 @@ def minor(matrix):
         # Iterate through the columns of the matrix
         for j in range(n):
             # Create a submatrix by excluding the current row and column
-            submatrix = [row[:j] + row[j + 1:] for row in (matrix[:i] + matrix[i + 1:])]
+            submatrix = [row[:j] + row[j + 1:]
+                         for row in (matrix[:i] + matrix[i + 1:])]
             # Calculate the determinant using the provided code
             minor_row.append(determinant(submatrix))
         # Append the minor row to the minor matrix
@@ -46,6 +53,7 @@ def minor(matrix):
 
     # Return the minor matrix
     return minor_mat
+
 
 def determinant(matrix):
     """
@@ -59,11 +67,12 @@ def determinant(matrix):
     """
     # Check if the input is a valid list of lists
     if not isinstance(matrix, list) or len(matrix) == 0:
-        raise TypeError("matrix must be a list of lists")
+        # Update the error message
+        raise ValueError("matrix must be a non-empty square matrix")
 
     # Check if all elements are lists
     if all(isinstance(i, list) for i in matrix) is False:
-        raise TypeError("matrix must be a list of lists")
+        raise ValueError("matrix must be a list of lists")
 
     # Get the number of rows in the matrix
     num_rows = len(matrix)
