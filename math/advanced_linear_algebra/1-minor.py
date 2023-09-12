c088afd6c1c0384ae3698851503d95de2340f14c
Your Name <you@example.com>
2023-09-11 21:53:11 -0500

    a function def minor(matrix): that calculates the minor matrix of a matrix

--- math/advanced_linear_algebra/1-minor.py
+++ math/advanced_linear_algebra/1-minor.py
@@ -3,6 +3,7 @@
 Advanced Linear Algebra Minor
 """
 
+
 def minor(matrix):
     """
     Calculates the minor matrix of a square matrix.
@@ -14,7 +15,12 @@ def minor(matrix):
         list of lists: The minor matrix.
     """
     # Check if matrix is a list of lists
-    if not isinstance(matrix, list) or not all(isinstance(row, list) for row in matrix):
+    if not isinstance(
+        matrix,
+        list) or not all(
+        isinstance(
+            row,
+            list) for row in matrix):
         raise TypeError("matrix must be a list of lists")
 
     # Get the size (number of rows) of the matrix
@@ -38,14 +44,17 @@ def minor(matrix):
         # Iterate through the columns of the matrix
         for j in range(n):
             # Create a submatrix by excluding the current row and column
-            submatrix = [row[:j] + row[j+1:] for row in (matrix[:i] + matrix[i+1:])]
-            # Calculate the determinant of the submatrix and append it to the minor row
+            submatrix = [row[:j] + row[j + 1:]
+                         for row in (matrix[:i] + matrix[i + 1:])]
+            # Calculate the determinant of the submatrix and append it to the
+            # minor row
             minor_row.append(determinant(submatrix))
         # Append the minor row to the minor matrix
         minor_mat.append(minor_row)
 
     # Return the minor matrix
     return minor_mat
+
 
 def determinant(matrix):
     """
@@ -80,9 +89,12 @@ def determinant(matrix):
 
     # Iterate through the first row (expansion by minors)
     for j in range(n):
-        # Calculate the cofactor (product of current element and minor determinant)
-        cofactor = matrix[0][j] * determinant([row[:j] + row[j+1:] for row in matrix[1:]])
-        # Add or subtract the cofactor to the determinant based on the column index
+        # Calculate the cofactor (product of current element and minor
+        # determinant)
+        cofactor = matrix[0][j] * \
+            determinant([row[:j] + row[j + 1:] for row in matrix[1:]])
+        # Add or subtract the cofactor to the determinant based on the column
+        # index
         det += cofactor if j % 2 == 0 else -cofactor
 
     # Return the determinant value
