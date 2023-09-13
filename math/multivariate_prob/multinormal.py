49cd3f527e969d9e94e9065e9cd635212243c3a5
Your Name <you@example.com>
2023-09-13 18:47:28 -0500

    public instance method def pdf(self, x): that calculates the PDF at a data point

--- math/multivariate_prob/multinormal.py
+++ math/multivariate_prob/multinormal.py
@@ -3,18 +3,14 @@
 update multinormal
 """
 
+# Import the NumPy library for numerical operations.
 import numpy as np
+
+# Define a class named MultiNormal for representing a Multivariate Normal
+# distribution.
 
 
 class MultiNormal:
-    """
-    Represents a Multivariate Normal distribution.
-
-    Attributes:
-        mean (np.ndarray): The mean vector of the dataset, shape (d, 1).
-        cov (np.ndarray): The covariance matrix of the dataset, shape (d, d).
-    """
-
     def __init__(self, data):
         """
         Initializes a Multivariate Normal distribution.
@@ -24,29 +20,52 @@ class MultiNormal:
 
         Raises:
             TypeError: If data is not a 2D numpy.ndarray.
-            ValueError: If n is less than 2
-            (data must contain multiple data points).
+            ValueError: If n is less than 2 (data must contain multiple data points).
 
         Attributes:
             mean (np.ndarray): The mean vector of the dataset, shape (d, 1).
-            cov (np.ndarray): The covariance matrix of the dataset,
-            shape (d, d).
+            cov (np.ndarray): The covariance matrix of the dataset, shape (d, d).
         """
-        # Check if data is a numpy.ndarray and has the correct shape
+        # Validate the input data.
+        self._validate_input(data)
+
+        # Calculate the mean vector and covariance matrix.
+        self.mean = np.mean(data, axis=1).reshape((-1, 1))
+        self.cov = self._calculate_covariance(data)
+
+    def _validate_input(self, data):
+        """
+        Validate the input data.
+
+        Args:
+            data (np.ndarray): The dataset to be validated.
+
+        Raises:
+            TypeError: If data is not a 2D numpy.ndarray.
+            ValueError: If n is less than 2 (data must contain multiple data points).
+        """
+        # Check if data is a 2D numpy array.
         if not isinstance(data, np.ndarray) or len(data.shape) != 2:
             raise TypeError("data must be a 2D numpy.ndarray")
 
-        # Check if data contains multiple data points
+        # Check if data contains multiple data points.
         if data.shape[1] < 2:
             raise ValueError("data must contain multiple data points")
 
-        # Calculate the mean vector of data
-        self.mean = np.mean(data, axis=1).reshape((-1, 1))
+    def _calculate_covariance(self, data):
+        """
+        Calculate the covariance matrix of the input data.
 
-        # Calculate the covariance matrix of data
+        Args:
+            data (np.ndarray): The dataset for which the covariance matrix is calculated.
+
+        Returns:
+            np.ndarray: The calculated covariance matrix.
+        """
+        # Calculate the covariance matrix.
         n = data.shape[1]
         data_centered = data - self.mean
-        self.cov = np.matmul(data_centered, data_centered.T) / (n - 1)
+        return np.matmul(data_centered, data_centered.T) / (n - 1)
 
     def pdf(self, x):
         """
@@ -62,20 +81,35 @@ class MultiNormal:
         Returns:
             float: The value of the PDF at the given data point.
         """
-        # Check if x is a numpy.ndarray
+        # Validate the input data point.
+        self._validate_data_point(x)
+
+        # Calculate the PDF value using the Multivariate Normal PDF formula.
+        d = self.mean.shape[0]
+        x_minus_mean = x - self.mean
+        cov_inv = np.linalg.inv(self.cov)
+        cov_det = np.linalg.det(self.cov)
+        exponent = -0.5 * np.matmul(x_minus_mean.T,
+                                    np.matmul(cov_inv, x_minus_mean))
+        denominator = np.sqrt((2 * np.pi) ** d * cov_det)
+        pdf_value = np.exp(exponent) / denominator
+        return pdf_value.flatten()[0]
+
+    def _validate_data_point(self, x):
+        """
+        Validate the input data point.
+
+        Args:
+            x (np.ndarray): The data point to be validated.
+
+        Raises:
+            TypeError: If x is not a numpy.ndarray.
+            ValueError: If x does not have the correct shape.
+        """
+        # Check if x is a numpy.ndarray.
         if not isinstance(x, np.ndarray):
             raise TypeError("x must be a numpy.ndarray")
 
-        # Check if x has the correct shape
+        # Check if x has the correct shape.
         if x.shape != self.mean.shape:
             raise ValueError(f"x must have the shape {self.mean.shape}")
-
-        # Calculate the PDF value using the Multivariate Normal PDF formula
-        d = self.mean.shape[0]
-        x_minus_mean = x - self.mean
-        exponent = -0.5 * \
-            np.matmul(x_minus_mean.T, np.matmul(np.linalg.inv(self.cov), x_minus_mean))
-        denominator = np.sqrt((2 * np.pi) ** d * np.linalg.det(self.cov))
-        pdf_value = np.exp(exponent) / denominator
-
-        return pdf_value
