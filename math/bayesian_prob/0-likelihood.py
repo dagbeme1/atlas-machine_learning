f51fc0e8e38e2df7a84983ef4ce1b08b1217147b
Your Name <you@example.com>
2023-09-13 21:23:39 -0500

    a function def likelihood(x, n, P): that calculates the likelihood of obtaining this data given various hypothetical probabilities of developing severe side effects

--- math/bayesian_prob/0-likelihood.py
+++ math/bayesian_prob/0-likelihood.py
@@ -4,9 +4,10 @@
 
 import numpy as np
 
+
 def likelihood(x, n, P):
     """
-    Calculate the likelihood of observing the data given various hypothetical probabilities
+    Calculate the likelihood of obtaining this data given various hypothetical probabilities
     of developing severe side effects.
 
     Args:
@@ -15,17 +16,18 @@ def likelihood(x, n, P):
         P (np.ndarray): Array of hypothetical probabilities.
 
     Returns:
-        np.ndarray: Array containing the likelihood of observing the data for each probability.
+        np.ndarray: Array containing the likelihood of obtaining the data for each probability.
 
     Raises:
         ValueError: If n is not a positive integer, if x is not a valid integer, if x > n,
                     or if any value in P is not in [0, 1].
         TypeError: If P is not a 1D numpy.ndarray.
     """
-    if not isinstance(n, int) or n <= 0:
+    if not isinstance(n, int) or n < 1:
         raise ValueError("n must be a positive integer")
     if not isinstance(x, int) or x < 0:
-        raise ValueError("x must be an integer that is greater than or equal to 0")
+        raise ValueError(
+            "x must be an integer that is greater than or equal to 0")
     if x > n:
         raise ValueError("x cannot be greater than n")
     if not isinstance(P, np.ndarray) or P.ndim != 1:
@@ -33,9 +35,11 @@ def likelihood(x, n, P):
     if np.any((P < 0) | (P > 1)):
         raise ValueError("All values in P must be in the range [0, 1]")
 
+    # Compute the binomial coefficient manually
+    binomial_coefficient = np.math.factorial(
+        n) / (np.math.factorial(x) * np.math.factorial(n - x))
+
     # Calculate the likelihood for each probability in P
-    binomial_coefficient = np.math.comb(n, x)
     likelihood_values = binomial_coefficient * (P ** x) * ((1 - P) ** (n - x))
 
     return likelihood_values
-
