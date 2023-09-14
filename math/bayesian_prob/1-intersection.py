7f22612a84e17a96f7bb58cf37bf745f8c1946f9
Your Name <you@example.com>
2023-09-14 14:17:17 -0500

    a function def intersection(x, n, P, Pr): that calculates the intersection of obtaining this data with the various hypothetical probabilities

--- math/bayesian_prob/1-intersection.py
+++ math/bayesian_prob/1-intersection.py
@@ -41,6 +41,10 @@ def intersection(x, n, P, Pr):
     if not isinstance(Pr, np.ndarray) or Pr.ndim != 1 or Pr.shape != P.shape:
         raise ValueError("Pr must be a numpy.ndarray with the same shape as P")
 
+    # Check if all values in Pr are within the range [0, 1]
+    if np.any((Pr < 0) | (Pr > 1)):
+        raise ValueError("All values in Pr must be in the range [0, 1]")
+
     # Check if the sum of values in Pr is approximately equal to 1
     if not np.isclose(np.sum(Pr), 1):
         raise ValueError("Pr must sum to 1")
