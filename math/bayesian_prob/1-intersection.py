cdd61b9833c9555aaa764d43a5a09634fb6393c0
Your Name <you@example.com>
2023-09-14 23:07:20 -0500

    a function def intersection(x, n, P, Pr): that calculates the intersection of obtaining this data with the various hypothetical probabilities

--- math/bayesian_prob/1-intersection.py
+++ math/bayesian_prob/1-intersection.py
@@ -7,7 +7,7 @@ import numpy as np
 
 def intersection(x, n, P, Pr):
     """
-    Calculate the likelihood of obtaining this data given various
+    Calculate the intersection of obtaining this data given various
     hypothetical probabilities of developing severe side effects.
 
     Args:
@@ -20,32 +20,32 @@ def intersection(x, n, P, Pr):
         the data for each probability.
 
     Raises:
-        ValueError: If n is not a positive integer,
-        if x is not a valid integer, if x > n,
-                    or if any value in P is not in [0, 1].
-        TypeError: If P is not a 1D numpy.ndarray.
+        ValueError: If n is not a positive integer, if x is not a valid integer,
+        if x > n, or if any value in P or Pr is not in [0, 1],
+        or if Pr does not sum to 1.
     """
-    if not isinstance(n, int) or n < 1:
+    if not isinstance(n, (int, float)) or n <= 0:
         raise ValueError("n must be a positive integer")
-    if not isinstance(x, int) or x < 0:
-        raise ValueError(
-            "x must be an integer that is greater than or equal to 0")
+
+    if not isinstance(x, (int, float)) or x < 0:
+        message = "x must be an integer that is greater than or equal to 0"
+        raise ValueError(message)
+
     if x > n:
         raise ValueError("x cannot be greater than n")
-    if not isinstance(P, np.ndarray) or P.ndim != 1 or P.shape != Pr.shape:
+
+    if not isinstance(P, np.ndarray) or len(P.shape) != 1 or P.shape[0] < 1:
         raise TypeError("P must be a 1D numpy.ndarray")
-    if np.any((P < 0) | (P > 1)):
+
+    if not isinstance(Pr, np.ndarray) or Pr.shape != P.shape:
+        raise TypeError("Pr must be a numpy.ndarray with the same shape as P")
+
+    if np.any(P > 1) or np.any(P < 0):
         raise ValueError("All values in P must be in the range [0, 1]")
 
-    # Ensure that Pr has the same shape as P
-    if not isinstance(Pr, np.ndarray) or Pr.ndim != 1 or Pr.shape != P.shape:
-        raise ValueError("Pr must be a numpy.ndarray with the same shape as P")
-
-    # Check if all values in Pr are within the range [0, 1]
-    if np.any((Pr < 0) | (Pr > 1)):
+    if np.any(Pr > 1) or np.any(Pr < 0):
         raise ValueError("All values in Pr must be in the range [0, 1]")
 
-    # Check if the sum of values in Pr is approximately equal to 1
     if not np.isclose(np.sum(Pr), 1):
         raise ValueError("Pr must sum to 1")
 
