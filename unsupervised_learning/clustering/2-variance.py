1470ef8ea5b280d07e26f2d434481ebce8022946
Your Name <you@example.com>
2023-09-25 03:57:11 -0500

    a function def variance(X, C): that calculates the total intra-cluster variance for a data set

--- unsupervised_learning/clustering/2-variance.py
+++ unsupervised_learning/clustering/2-variance.py
@@ -1,29 +1,32 @@
 #!/usr/bin/env python3
-"""contains the variance function"""
+"""Contains the variance function"""
 
 import numpy as np
 
+
 def variance(X, C):
     """
-    Calculate the total intra-cluster variance for a given data set.
+    Calculate the total intra-cluster variance for a data set.
 
     Args:
-        X (numpy.ndarray): The dataset with shape (n_samples, n_features).
-        C (numpy.ndarray): The centroid means for each cluster with shape (n_clusters, n_features).
+        X (numpy.ndarray): The data set with shape (n, d).
+        C (numpy.ndarray): The centroid means for each cluster with shape (k, d).
 
     Returns:
         float: The total intra-cluster variance, or None on failure.
     """
     try:
-        n_samples, n_features = X.shape
-        n_clusters, _ = C.shape
+        n_clusters, n_features = C.shape
 
-        # Calculate the squared Euclidean distances between data points and centroids
-        X_broadcasted = X[:, np.newaxis, :]
-        squared_distances = np.sum((X_broadcasted - C) ** 2, axis=2)
+        # Calculate the squared Euclidean distances between data points and
+        # centroids
+        centroids_squared_norm = np.sum(C ** 2, axis=1)[:, np.newaxis]
+        data_squared_norm = np.sum(X ** 2, axis=1)
+        dot_product = np.matmul(C, X.T)
+        squared_distances = centroids_squared_norm + data_squared_norm - 2 * dot_product
 
         # Find the minimum squared distance for each data point
-        min_squared_distances = np.min(squared_distances, axis=1)
+        min_squared_distances = np.amin(squared_distances, axis=0)
 
         # Calculate the total intra-cluster variance
         total_variance = np.sum(min_squared_distances)
