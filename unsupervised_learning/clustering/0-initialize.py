849941c2c4c565d131600a5255dce12f83d5e992
Your Name <you@example.com>
2023-09-25 00:02:30 -0500

    a function def initialize(X, k): that initializes cluster centroids for K-means

--- unsupervised_learning/clustering/0-initialize.py
+++ unsupervised_learning/clustering/0-initialize.py
@@ -5,34 +5,35 @@
 
 import numpy as np
 
+
 def initialize(X, k):
     """
     Initializes cluster centroids for K-means.
 
     Args:
-        X (numpy.ndarray): The input dataset with shape (n_samples, n_features).
+        X (numpy.ndarray): The dataset with shape (n, d).
         k (int): The number of clusters.
 
     Returns:
-        numpy.ndarray: The initialized centroids for each cluster.
+        numpy.ndarray: The initialized centroids for each cluster, or None on failure.
     """
+
     # Check if X is a numpy array with two dimensions
     if not isinstance(X, np.ndarray) or X.ndim != 2:
         return None
 
-    # Get the number of data points (n_samples) and the dimensionality of each
-    # data point (n_features)
+    # Get the number of data points (n) and the dimensionality of each data point (d)
     n_samples, n_features = X.shape
 
     # Check if k is a positive integer within a valid range
     if not isinstance(k, int) or k <= 0 or k > n_samples:
         return None
 
-    # Compute the minimum and maximum values for each feature
-    feature_min = np.min(X, axis=0)
-    feature_max = np.max(X, axis=0)
+    # Initialize centroids as an array of zeros
+    centroids = np.zeros((k, n_features))
 
-    # Initialize centroids using random values within the data range
-    centroids = np.random.uniform(feature_min, feature_max, (k, n_features))
+    # Randomly choose k distinct data points as initial centroids
+    indices = np.random.choice(n_samples, k, replace=False)
+    centroids = X[indices]
 
     return centroids
