3d4482ac535fef4183ac10462b6fefaba4fbec5a
Your Name <you@example.com>
2023-09-25 06:08:54 -0500

    a function def initialize(X, k): that initializes variables for a Gaussian Mixture Model

--- unsupervised_learning/clustering/4-initialize.py
+++ unsupervised_learning/clustering/4-initialize.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 """
-a function to initialize variables for a Gaussian Mixture Model (GMM)
+A function to initialize variables for a Gaussian Mixture Model (GMM)
 """
+
 import numpy as np
 # Import kmeans function
 kmeans = __import__('1-kmeans').kmeans
@@ -9,46 +10,49 @@ kmeans = __import__('1-kmeans').kmeans
 
 def initialize(X, k):
     """
-    Initializes variables for a Gaussian Mixture Model
+    Initializes variables for a Gaussian Mixture Model (GMM).
 
     Args:
-        X (numpy.ndarray): The input dataset with
-        shape (n_samples, n_features).
+        X (numpy.ndarray): The input dataset with shape (n_samples, n_features).
         k (int): The number of clusters.
 
     Returns:
         Tuple[numpy.ndarray, numpy.ndarray, numpy.ndarray]:
         A tuple containing:
-            - priors: A numpy.ndarray of shape (k,) containing the
-            priors for each cluster, initialized evenly.
-            - means: A numpy.ndarray of shape (k, n_features) containing the
-            centroid means for each cluster, initialized with K-means.
-            - covariances: A numpy.ndarray of shape (k, n_features, n_features)
-            containing the covariance matrices for each cluster,
-            initialized as identity matrices.
+            - priors: A numpy.ndarray of shape (k,)
+            containing the priors for each cluster, initialized evenly.
+            - means: A numpy.ndarray of shape (k, n_features)
+            containing the centroid means for each cluster,
+            initialized with K-means.
+            - covariances: A numpy.ndarray of shape
+            (k, n_features, n_features) containing
+            the covariance matrices for each cluster, initialized as identity matrices.
             Returns (None, None, None) on failure.
     """
-    # Check if X is a valid ndarray with two dimensions
+    # Check if X is a numpy array and has 2 dimensions (n_samples, n_features)
     if not isinstance(X, np.ndarray) or X.ndim != 2:
         return None, None, None
 
-    # Get the number of data points (n_samples) and
-    # the number of features (n_features)
+    # Get the number of samples (n_samples) and the
+    # number of features (n_features) from the input dataset X
     n_samples, n_features = X.shape
 
-    # Check if k is a valid integer within the valid range
+    # Check if k is a positive integer within the valid range
     if not isinstance(k, int) or k <= 0 or k > n_samples:
         return None, None, None
 
-    # Initialize the "priors" array of shape (k,) with
-    # equal priors for each cluster
+    # Initialize the "priors" array of shape (k,)
+    # containing the priors for each cluster, initialized evenly
     priors = np.tile(1 / k, (k,))
 
-    # Initialize the "means" array of shape (k, n_features) using K-means
+    # Initialize the "means" array of shape (k, n_features)
+    # containing the centroid means for each cluster, initialized with K-means
     cluster_means, cluster_assignments = kmeans(X, k)
 
-    # Initialize the "covariances" array of
-    # shape (k, n_features, n_features) as identity matrices
-    cluster_covariances = np.tile(np.identity(n_features), (k, 1, 1))
+    # Initialize the "covariances" array of shape (k, n_features, n_features)
+    # containing the covariance matrices for each cluster,
+    # initialized as identity matrices
+    identity_matrix = np.identity(n_features)
+    covariances = np.tile(identity_matrix, (k, 1, 1))
 
-    return priors, cluster_means, cluster_covariances
+    return priors, cluster_means, covariances
