3566ecdfe5130d288120a781b3915865e7cde52b
Your Name <you@example.com>
2023-09-25 02:56:52 -0500

    a function def variance(X, C): that calculates the total intra-cluster variance for a data set

---
+++ unsupervised_learning/clustering/2-variance.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""
+2-variance
+"""
+
+import numpy as np
+
+
+def variance(X, C):
+    """
+    Calculate the total intra-cluster variance for a data set.
+
+    Args:
+        X (numpy.ndarray): The input data set with shape (n, d).
+        C (numpy.ndarray): The centroid means for each cluster with shape (k, d).
+
+    Returns:
+        float: The total intra-cluster variance, or None on failure.
+    """
+    try:
+        k, d = C.shape
+
+        # Calculate the squared Euclidean distances between data points and
+        # centroids
+        distances = np.sum((X[:, np.newaxis] - C) ** 2, axis=2)
+
+        # Find the minimum distance for each data point
+        min_distances = np.min(distances, axis=1)
+
+        # Calculate the total intra-cluster variance
+        var = np.sum(min_distances)
+
+        return var
+
+    except Exception:
+        return None
