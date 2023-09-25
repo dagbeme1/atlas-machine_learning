0ded7ed0b5edd9ca09e8f8c63c80181f8a0eb9fb
Your Name <you@example.com>
2023-09-25 12:24:40 -0500

    a function def expectation_maximization(X, k, iterations=1000, tol=1e-5, verbose=False): that performs the expectation maximization for a GMM

--- unsupervised_learning/clustering/8-EM.py
+++ unsupervised_learning/clustering/8-EM.py
@@ -11,28 +11,36 @@ initialize = __import__('4-initialize').initialize
 expectation = __import__('6-expectation').expectation
 maximization = __import__('7-maximization').maximization
 
+
 def expectation_maximization(X, k, iterations=1000, tol=1e-5, verbose=False):
     """
-    Perform the Expectation-Maximization (EM) algorithm for a Gaussian Mixture Model (GMM).
+    Perform the Expectation-Maximization (EM) algorithm
+    for a Gaussian Mixture Model (GMM).
 
     Args:
         X (numpy.ndarray): Data points of shape (n_samples, n_features).
         k (int): Number of clusters.
         iterations (int): Maximum number of iterations for the algorithm.
         tol (float): Tolerance of the log likelihood, used for early stopping.
-        verbose (bool): Determines if information about the algorithm should be printed.
+        verbose (bool): Determines if information about
+        the algorithm should be printed.
 
     Returns:
-        Tuple[numpy.ndarray, numpy.ndarray, numpy.ndarray, numpy.ndarray, float]:
-        - updated_priors (numpy.ndarray): Updated priors for each cluster of shape (k,).
-        - updated_means (numpy.ndarray): Updated centroid means for each cluster of shape (k, n_features).
-        - updated_covariances (numpy.ndarray): Updated covariance matrices for each cluster of shape (k, n_features, n_features).
-        - posterior_probs (numpy.ndarray): Probabilities for each data point in each cluster of shape (k, n_samples).
+        Tuple[numpy.ndarray, numpy.ndarray, numpy.
+        ndarray, numpy.ndarray, float]:
+        - updated_priors (numpy.ndarray):
+        Updated priors for each cluster of shape (k,).
+        - updated_means (numpy.ndarray): Updated centroid
+        means for each cluster of shape (k, n_features).
+        - updated_covariances (numpy.ndarray): Updated covariance matrices
+        for each cluster of shape (k, n_features, n_features).
+        - posterior_probs (numpy.ndarray): Probabilities for each
+        data point in each cluster of shape (k, n_samples).
         - log_likelihood (float): Log likelihood of the model.
 
     Returns (None, None, None, None, None) on failure.
     """
-    
+
     if not isinstance(X, np.ndarray) or len(X.shape) != 2:
         return None, None, None, None, None
     if not isinstance(k, int) or k <= 0 or X.shape[0] < k:
@@ -55,7 +63,8 @@ def expectation_maximization(X, k, iterations=1000, tol=1e-5, verbose=False):
                 iteration, likelihood.round(5)))
 
         priors, means, covariances = maximization(X, responsibilities)
-        responsibilities, likelihood = expectation(X, priors, means, covariances)
+        responsibilities, likelihood = expectation(
+            X, priors, means, covariances)
 
         if abs(prev_likelihood - likelihood) <= tol:
             break
