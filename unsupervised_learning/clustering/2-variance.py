6c0c907b63430f9530cd9ebb2f9ce4568057cd2e
Your Name <you@example.com>
2023-09-25 04:04:18 -0500

    a function def variance(X, C): that calculates the total intra-cluster variance for a data set

--- unsupervised_learning/clustering/2-variance.py
+++ unsupervised_learning/clustering/2-variance.py
@@ -1,35 +1,34 @@
 #!/usr/bin/env python3
-"""Contains the variance function"""
+"""
+variance function
+"""
 
 import numpy as np
-
 
 def variance(X, C):
     """
     Calculate the total intra-cluster variance for a data set.
 
     Args:
-        X (numpy.ndarray): The data set with shape (n, d).
-        C (numpy.ndarray): The centroid means for each cluster with shape (k, d).
+        X (numpy.ndarray): The data set with shape (n_samples, n_features).
+        C (numpy.ndarray): The centroid means for each cluster with shape (n_clusters, n_features).
 
     Returns:
         float: The total intra-cluster variance, or None on failure.
     """
     try:
-        n_clusters, n_features = C.shape
+        # Get the dimensions of the input arrays
+        num_clusters, num_features = C.shape
+        num_samples, _ = X.shape
 
-        # Calculate the squared Euclidean distances between data points and
-        # centroids
-        centroids_squared_norm = np.sum(C ** 2, axis=1)[:, np.newaxis]
+        # Compute the squared Euclidean distances between data points and centroids
+        centroid_squared_norm = np.sum(C ** 2, axis=1)[:, np.newaxis]
         data_squared_norm = np.sum(X ** 2, axis=1)
         dot_product = np.matmul(C, X.T)
-        squared_distances = centroids_squared_norm + data_squared_norm - 2 * dot_product
-
-        # Find the minimum squared distance for each data point
-        min_squared_distances = np.amin(squared_distances, axis=0)
+        squared_distances = centroid_squared_norm - 2 * dot_product + data_squared_norm
 
         # Calculate the total intra-cluster variance
-        total_variance = np.sum(min_squared_distances)
+        total_variance = np.sum(np.amin(squared_distances, axis=0))
 
         return total_variance
 
