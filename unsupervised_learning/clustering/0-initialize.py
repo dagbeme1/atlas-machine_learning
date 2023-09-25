2834cfb699543ed1ba747b9cd0ec584aa540a522
Your Name <you@example.com>
2023-09-25 00:00:00 -0500

    a function def initialize(X, k): that initializes cluster centroids for K-means

--- unsupervised_learning/clustering/0-initialize.py
+++ unsupervised_learning/clustering/0-initialize.py
@@ -33,6 +33,6 @@ def initialize(X, k):
     feature_max = np.max(X, axis=0)
 
     # Initialize centroids using random values within the data range
-    centroid = np.random.uniform(feature_min, feature_max, (k, n_features))
+    centroids = np.random.uniform(feature_min, feature_max, (k, n_features))
 
-    return centroid
+    return centroids
