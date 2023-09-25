211fd74a32a2ef4a069d9fb7bea44dfde2718c8e
Your Name <you@example.com>
2023-09-25 06:58:59 -0500

    a function def initialize(X, k): that initializes variables for a Gaussian Mixture Model

--- unsupervised_learning/clustering/4-initialize.py
+++ unsupervised_learning/clustering/4-initialize.py
@@ -4,7 +4,7 @@ A function to initialize variables for a Gaussian Mixture Model (GMM)
 """
 
 import numpy as np
-kmeans = __import__('1-kmeans').kmeans
+kmeans = __import__('1-kmeans').kmeans  # Import the kmeans function
 
 
 def initialize(X, k):
@@ -12,7 +12,9 @@ def initialize(X, k):
     Initializes cluster centroids for K-means.
 
     Args:
-        X (numpy.ndarray): The input dataset with shape (n_samples, n_features) containing the dataset for K-means clustering.
+        X (numpy.ndarray):
+        The input dataset with shape (n_samples, n_features)
+        containing the dataset for K-means clustering.
             - n_samples: The number of data points.
             - n_features: The number of dimensions for each data point.
         k (int): A positive integer containing the number of clusters.
@@ -20,9 +22,13 @@ def initialize(X, k):
     Returns:
         Tuple[numpy.ndarray, numpy.ndarray, numpy.ndarray]:
         A tuple containing:
-            - priors (numpy.ndarray): The priors for each cluster, initialized evenly.
-            - centroids (numpy.ndarray): The centroid means for each cluster, initialized with K-means.
-            - covariances (numpy.ndarray): The covariance matrices for each cluster, initialized as identity matrices.
+            - priors (numpy.ndarray):
+            The priors for each cluster, initialized evenly.
+            - centroids (numpy.ndarray):
+            The centroid means for each cluster, initialized with K-means.
+            - covariances (numpy.ndarray):
+            The covariance matrices for each cluster,
+            initialized as identity matrices.
             Returns (None, None, None) on failure.
     """
     if not isinstance(X, np.ndarray) or len(X.shape) != 2:
@@ -30,21 +36,19 @@ def initialize(X, k):
     if not isinstance(k, int) or k <= 0 or k >= X.shape[0]:
         return None, None, None
 
-    # Get the dimensions of the input dataset X
     n_samples, n_features = X.shape
-
-    # Initialize the "priors" array of shape (k,) containing the priors for
-    # each cluster, initialized evenly
+    # Get the dimensions of the input dataset X
     priors = np.tile(1 / k, (k,))
-
-    # Initialize the "centroids" array of shape (k, n_features) containing the
-    # centroid means for each cluster, initialized with K-means
+    # Initialize the "priors" array of shape (k,) containing
+    # the priors for each cluster, initialized evenly
     centroids, _ = kmeans(X, k)
-
-    # Initialize the "covariances" array of shape (k, n_features, n_features)
-    # containing the covariance matrices for each cluster, initialized as
-    # identity matrices
+    # Initialize the "centroids" array of shape (k, n_features) containing
+    # the centroid means for each cluster, initialized with K-means
     identity_matrix = np.identity(n_features)
+    # Initialize the identity matrix of shape (n_features, n_features)
     covariances = np.tile(identity_matrix, (k, 1, 1))
+    # Initialize the "covariances" array of shape (k, n_features, n_features)
+    # containing the covariance matrices for each cluster,
+    # initialized as identity matrices
 
     return priors, centroids, covariances
