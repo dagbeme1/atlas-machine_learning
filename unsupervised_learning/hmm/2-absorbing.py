8805042ed655b772339b418b7bb070603e15d77b
Your Name <you@example.com>
2023-10-04 11:35:42 -0500

    the function def absorbing(P): that determines if a markov chain is absorbing

--- unsupervised_learning/hmm/2-absorbing.py
+++ unsupervised_learning/hmm/2-absorbing.py
@@ -1,84 +1,63 @@
 #!/usr/bin/env python3
-"""
-the function def absorbing(P): that determines
-if a markov chain is absorbing
-"""
+"""Contains functions for Markov chains analysis."""
+
 import numpy as np
 
 
 def markov_chain(P, s, t=1):
     """
-    Determines the probability of a Markov chain being in a particular state
-    after a specified number of iterations.
+    Determines the probability of a Markov chain being
+    in a particular state after a specified number of iterations.
 
     Args:
-        P (numpy.ndarray): Transition matrix of shape (n, n), where P[i, j]
-            is the probability of transitioning from state i to state j.
-        s (numpy.ndarray): Initial state probabilities of shape (1, n).
+        P (numpy.ndarray): Transition matrix of shape (n, n),
+            where P[i, j] is the probability of transitioning from state i to state j.
+        s (numpy.ndarray): Initial probability distribution of shape (1, n),
+            representing the probability of starting in each state.
         t (int): Number of iterations.
 
     Returns:
-        numpy.ndarray: Probability of being in
-        a specific state after t iterations,
+        numpy.ndarray: Probability distribution after t iterations,
         or None on failure.
     """
     if not isinstance(P, np.ndarray) or P.ndim != 2:
         return None
-    n = P.shape[0]
-
-    if P.shape != (n, n) or not np.allclose(np.sum(P, axis=1), np.ones(n)):
+    if P.shape[0] != P.shape[1]:
         return None
-
-    if not isinstance(
-            s,
-            np.ndarray) or s.shape != (
-            1,
-            n) or not np.allclose(
-                np.sum(
-                    s,
-                    axis=1),
-            [1]):
+    if not isinstance(s, np.ndarray) or s.ndim != 2:
         return None
-
+    if s.shape[0] != 1 or s.shape[1] != P.shape[1]:
+        return None
     if not isinstance(t, int) or t < 0:
+        return None
+    n = P.shape[0]
+    if not np.isclose(np.sum(P, axis=1), np.ones(n))[0]:
         return None
 
     Pt = np.linalg.matrix_power(P, t)
     Ps = np.matmul(s, Pt)
-
     return Ps
 
 
 def regular(P):
     """
-    Determines the steady-state probabilities of a regular Markov chain.
+    Determines if a Markov chain is regular.
 
     Args:
-        P (numpy.ndarray): Transition matrix of shape (n, n), where P[i, j]
-            is the probability of transitioning from state i to state j.
+        P (numpy.ndarray): Transition matrix of shape (n, n),
+            where P[i, j] is the probability of transitioning from state i to state j.
 
     Returns:
-        numpy.ndarray: Steady-state probabilities of shape (1, n),
-        or None on failure.
+        bool: True if the Markov chain is regular, or False on failure.
     """
     if not isinstance(P, np.ndarray) or P.ndim != 2:
-        return None
+        return False
     n = P.shape[0]
-
     if P.shape != (n, n) or not np.allclose(np.sum(P, axis=1), np.ones(n)):
-        return None
-
-    s = np.full((1, n), 1 / n)
-    Pk = np.copy(P)
-
-    while True:
-        Pk = np.matmul(Pk, P)
-        s_new = np.matmul(s, P)
-
-        if np.allclose(s, s_new):
-            return s
-
-        s = s_new
+        return False
+    if np.all(np.diag(P) == 1):
+        return False
+    return True
 
 
 def absorbing(P):
@@ -86,26 +65,21 @@ def absorbing(P):
     Determines if a Markov chain is absorbing.
 
     Args:
-        P (numpy.ndarray): Transition matrix of shape (n, n), where P[i, j]
-            is the probability of transitioning from state i to state j.
+        P (numpy.ndarray): Transition matrix of shape (n, n),
+            where P[i, j] is the probability of transitioning from state i to state j.
 
     Returns:
-        bool: True if it is absorbing, or False on failure.
+        bool: True if the Markov chain is absorbing, or False on failure.
     """
     if not isinstance(P, np.ndarray) or P.ndim != 2:
         return False
     n = P.shape[0]
-
     if P.shape != (n, n) or not np.allclose(np.sum(P, axis=1), np.ones(n)):
         return False
 
     if np.all(np.diag(P) == 1):
         return True
 
-    return route_check(P, n)
-
-
-def route_check(P, n):
     absorbing_states = np.where(np.diag(P) == 1)
     rows = P[absorbing_states[0]]
     account = np.sum(rows, axis=0)
