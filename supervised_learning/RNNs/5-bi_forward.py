d5b44711f23857d4625e78910508ab8cfd7c0433
Your Name <you@example.com>
2024-03-13 15:10:47 -0500

    the class BidirectionalCell that represents a bidirectional cell of an RNN,Creates the public instance attributes Whf, Whb, Wy, bhf, bhb, by that represent the weights and biases of the cell

---
+++ supervised_learning/RNNs/5-bi_forward.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+"""
+the class BidirectionalCell that represents a bidirectional cell of an RNN
+"""
+import numpy as np
+
+
+class BidirectionalCell:
+    """
+    Represents a bidirectional cell of an RNN
+    """
+
+    def __init__(self, i, h, o):
+        """
+        Class constructor
+
+        Args:
+            i (int): Dimensionality of the data.
+            h (int): Dimensionality of the hidden states.
+            o (int): Dimensionality of the outputs.
+        """
+        # Initialize weights and biases for forward and backward directions
+        self.Whf = np.random.randn(h + i, h)
+        self.Whb = np.random.randn(h + i, h)
+        self.Wy = np.random.randn(h * 2, o)
+        self.bhf = np.zeros((1, h))
+        self.bhb = np.zeros((1, h))
+        self.by = np.zeros((1, o))
+
+    def forward(self, h_prev, x_t):
+        """
+        Calculates the hidden state in the forward direction for one time step.
+
+        Args:
+            h_prev (numpy.ndarray): Previous hidden state.
+            x_t (numpy.ndarray): Data input for the cell.
+
+        Returns:
+            numpy.ndarray: Next hidden state.
+        """
+        # Calculate next hidden state in the forward direction
+        h_next = np.tanh(np.dot(np.hstack((h_prev, x_t)), self.Whf) + self.bhf)
+        return h_next
