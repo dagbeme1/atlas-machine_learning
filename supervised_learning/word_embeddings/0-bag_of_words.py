bf3dd6bf9c89a19338da9822d0228a52b88cbf50
Your Name <you@example.com>
2024-03-27 13:48:19 -0500

    had to pip install scikit-lear and also make some upgrades

--- supervised_learning/word_embeddings/0-bag_of_words.py
+++ supervised_learning/word_embeddings/0-bag_of_words.py
@@ -3,6 +3,26 @@
 Enhanced Bag of Words Embedding
 """
 
+
+class CountVectorizer:
+    def __init__(self, vocabulary=None):
+        self.vocabulary_ = vocabulary
+
+    def fit_transform(self, sentences):
+        if self.vocabulary_ is None:
+            # If no vocabulary is provided, create it from sentences
+            self.vocabulary_ = sorted(set(word for sentence in sentences for word in sentence.split()))
+
+        # Transform sentences into a matrix of token counts
+        embeddings = []
+        for sentence in sentences:
+            embedding = [sentence.split().count(word) for word in self.vocabulary_]
+            embeddings.append(embedding)
+
+        return embeddings
+
+    def get_feature_names(self):
+        return self.vocabulary_
 # from sklearn.feature_extraction.text import CountVectorizer
 
 
