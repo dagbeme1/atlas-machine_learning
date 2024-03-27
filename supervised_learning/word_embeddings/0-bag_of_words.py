4db4eed2aa48a6f22f8d75c66fc87fc4e0e8af03
Your Name <you@example.com>
2024-03-27 13:58:46 -0500

    fixed the countvectorize problem i was running into earlier

--- supervised_learning/word_embeddings/0-bag_of_words.py
+++ supervised_learning/word_embeddings/0-bag_of_words.py
@@ -3,26 +3,7 @@
 Enhanced Bag of Words Embedding
 """
 
-
-class CountVectorizer:
-    def __init__(self, vocabulary=None):
-        self.vocabulary_ = vocabulary
-
-    def fit_transform(self, sentences):
-        if self.vocabulary_ is None:
-            # If no vocabulary is provided, create it from sentences
-            self.vocabulary_ = sorted(set(word for sentence in sentences for word in sentence.split()))
-
-        # Transform sentences into a matrix of token counts
-        embeddings = []
-        for sentence in sentences:
-            embedding = [sentence.split().count(word) for word in self.vocabulary_]
-            embeddings.append(embedding)
-
-        return embeddings
-
-    def get_feature_names(self):
-        return self.vocabulary_
+import sklearn.feature_extraction.text
 # from sklearn.feature_extraction.text import CountVectorizer
 
 
@@ -44,10 +25,10 @@ def bag_of_words(sentences, vocab=None):
             features (list): A list of the features used for embeddings
     """
 
-    vectorizer = CountVectorizer(vocabulary=vocab)
+    vectorizer = sklearn.feature_extraction.text.CountVectorizer()
     X = vectorizer.fit_transform(sentences)
 
     embeddings = X.toarray()
-    features = vectorizer.get_feature_names()
+    features = vectorizer.get_feature_names_out()
 
     return embeddings, features
