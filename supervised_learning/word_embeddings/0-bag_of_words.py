1e1769671151baf43226de697ade19108177640b
Your Name <you@example.com>
2024-03-27 12:50:42 -0500

    a function def bag_of_words(sentences, vocab=None): that creates a bag of words embedding matrix

--- supervised_learning/word_embeddings/0-bag_of_words.py
+++ supervised_learning/word_embeddings/0-bag_of_words.py
@@ -12,11 +12,13 @@ def bag_of_words(sentences, vocab=None):
 
     Args:
         sentences (list): A list of sentences to analyze
-        vocab (list, optional): A list of the vocabulary words to use for the analysis
+        vocab (list, optional): A list of the vocabulary words 
+        to use for the analysis
 
     Returns:
         tuple: A tuple containing embeddings and features
-            embeddings (numpy.ndarray): A numpy array of shape (s, f) containing the embeddings
+            embeddings (numpy.ndarray): A numpy array of shape 
+            (s, f) containing the embeddings
                 s is the number of sentences in sentences
                 f is the number of features analyzed
             features (list): A list of the features used for embeddings
