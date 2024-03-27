fb03ae9e5b445ac54c990d92012bf3cf1bbee9b2
Your Name <you@example.com>
2024-03-27 13:31:20 -0500

    had to pip install scikit-lear and also make some upgrades

--- supervised_learning/word_embeddings/0-bag_of_words.py
+++ supervised_learning/word_embeddings/0-bag_of_words.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-Creates a bag of words embedding matrix
+Enhanced Bag of Words Embedding
 """
 from sklearn.feature_extraction.text import CountVectorizer
 
@@ -22,9 +22,11 @@ def bag_of_words(sentences, vocab=None):
                 f is the number of features analyzed
             features (list): A list of the features used for embeddings
     """
+
     vectorizer = CountVectorizer(vocabulary=vocab)
     X = vectorizer.fit_transform(sentences)
-    features = vectorizer.get_feature_names()
+
     embeddings = X.toarray()
+    features = vectorizer.get_feature_names()
 
     return embeddings, features
