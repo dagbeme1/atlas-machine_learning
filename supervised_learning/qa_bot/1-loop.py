d5c7b30a677e873dba1bf1e1ccbad9685bb819f5
Your Name <you@example.com>
2024-04-16 23:58:03 -0500

    a script that takes in input from the user with the prompt Q: and prints A: as a response. If the user inputs exit, quit, goodbye, or bye, case insensitive, print A: Goodbye and exit

---
+++ supervised_learning/qa_bot/1-loop.py
@@ -0,0 +1,15 @@
+"""
+a script that takes in input from the user with the prompt Q:
+and prints A: as a response. If the user inputs exit, quit, goodbye, or bye,
+case insensitive, print A: Goodbye and exit
+"""
+
+while True:
+    question = input("Q: ")
+    words = ["exit", "quit", "goodbye", "bye"]
+
+    if question.lower().strip() in words:
+        print("A: Goodbye")
+        break
+    else:
+        print("A: ")
