9ec01c1cf36ac478d7592cfc27756cd3ef80d996
Your Name <you@example.com>
2024-06-27 09:32:34 -0500

    a SQL script that creates an index idx_name_first on the table names and the first letter of name.
    Requirements:
    
    Import this table dump: names.sql.zip
    Only the first letter of name must be indexed

--- pipeline/databases/100-index_my_names.sql
+++ pipeline/databases/100-index_my_names.sql
@@ -1,5 +1,41 @@
--- Import the table dump
-SOURCE names.sql;
+-- Step 1: Drop the table if it exists
+DROP TABLE IF EXISTS `names`;
 
--- Create an index on the first letter of the name
-CREATE INDEX idx_name_first ON names((LEFT(name, 1)));
+-- Step 2: Create the table
+CREATE TABLE `names` (
+  `name` VARCHAR(255) DEFAULT NULL,
+  `score` INT(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Step 3: Insert data into the table
+-- Continuation of inserting values into the 'names' table
+INSERT INTO `names` VALUES
+('cybwnbrzvy',95),('kbjapvacvw',137),('zphpgamphc',194),('nsfnwpolbi',157),('rkebmatnom',60),('ntzppnipvi',162),('tgfvtbjtyg',173),('ghhtfwzpun',191),('uuohrattqo',79),('erzvprerfl',128),
+('wwrxvjiscy',199),('zwjgkiwyue',54),('xlkubygibe',59),('thnhqdvjfx',81),('vgrtkctypq',107),('wpvipkepir',142),('hskwantvkw',116),('bqhxweaaal',70),('ebtcrrmmmu',111),('bpwyrobljh',104),
+('ivrrbfelhb',168),('xrpptlthrz',194),('tsugbvpqvm',90),('dtstufujrf',134),('earpouzwhb',192),('dbkgbilgav',167),('wybbmsiuyx',105),('egriimmrmp',71),('azmzxeylqi',162),('cjvggmalab',154),
+('spfskdubvs',140),('eaqnftdxdq',55),('gujbqirqmv',158),('bkgurguuul',141),('dbwlxgtuqe',92),('doldbxqadv',185),('gdhwxyntny',73),('yeucyhslzr',146),('jsfjfsfyec',122),('wtxpahypwq',91),
+('bjfsabqgvr',73),('mlzzphiszc',117),('qbevzuicvl',197),('btftnxmqge',173),('cxnabvparj',168),('ghwyvsgtgg',172),('sobbolzbui',187),('qvangamclw',128),('gwegvhqxmt',137),('xqekcxfzzo',89),
+('iodbhdofek',97),('pvqwduzmfc',194),('twnyygxdhq',164),('njtuvdetfm',79),('zkyhzghiuv',133),('aacbeynhqk',102),('ntoblapqpd',94),('cllxtokjrw',178),('qmrzpuwiwg',118),('tpmptjkvzb',104),
+('purjcdapuu',177),('uhowzmajow',77),('hmcysilskx',185),('voofogxfvu',139),('hyesesqlok',130),('vicabwxlpw',142),('gqeuxnrdie',55),('smylzvkycs',112),('gvwxafausp',76),('hwokzspghh',185),
+('ufmzxdbhhs',139),('cnzgjvpzow',95),('xhfodsgmny',54),('zkghnjtcwm',135),('gzffolvlqw',168),('hszmpsubvt',161),('ogfobrdbjm',191),('ilpopaedto',128),('hexagtrvvy',65),('yslttydxvy',106),
+('mxmpkvvpgn',94),('ibngspkrsf',127),('blrkdhorhp',113),('rldkrrhbxr',55),('yahfvhduma',133),('ueihtewcrs',97),('wzsgmhtvlw',71),('uzzcukemok',157),('zhalthcozt',70),('fwwnjsamzw',175),
+('wdrwqiomhv',195),('lxvskqyfra',86),('sjmqxehvif',164),('xjsunicsst',127),('bxuukgmmlj',90),('ezlvswumzo',60),('lqindgxtma',149),('wglwzquwpe',56),('otduhcoxvw',102),('cmlvkxvzzn',57),
+('lnwtsyulqh',59),('vorpytdnwn',193),('cltyrihdpm',170),('tefumtucvq',51),('vjkapopvod',177),('xnfsfonmmi',168),('vovjmylhkf',70),('ruzcluorty',191),('jvcpvatrkv',65),('nbmuhuytch',180),
+('abdnvrvubm',143),('wwcbahjvip',125),('tucaeomzbz',66),('yvdskmclwi',138),('fynjstscag',61),('zxpachhslz',96),('sagnizqffa',58),('qqfpoiifpa',138),('zpyligdzep',138),('dujzvcbbvp',170),
+('ztnmocsrvl',180),('ysdfqkptno',169),('sjovxrkijv',58),('vwykpksesp',159),('xnxqmithpb',188),('mutfvmuqgt',199),('mwspbrstmq',185),('wrxzupklqa',151),('zmfwjdsyhr',91),('kzkbusovqs',155),
+('mvawyikvdc',156),('fbvelqzhch',144),('rfpbxggaly',85),('ibuznxqbya',150),('nkddgsxwgi',66),('fgaczsfiyf',178),('ckkzusifae',150),('hpjrawcmhe',77),('vvsvbeoygp',56),('bjgmqnrrfj',197),
+('qidfvazkwj',61),('xnzjwzzptl',169),('pubipvkysz',64),('amlracmfkw',105),('lgaaunkbyw',127),('ybfxsebrad',113),('asschtlsti',72),('sbkdtiwerd',76),('tpobuwxggp',75),('wxwpiiwhlc',61),
+('abatmrgmmn',136),('wyjlflkijq',82),('faecjrwihh',189),('zadiyrfosd',116),('mfvgzirlhd',54),('rfmdmzbovr',88),('uxnkbxqecr',70),('duybmdhmuf',186),('ngrnbbzvrr',112),('rveeazeeiq',112),
+('xlbssmdnht',81),('ffxwwrfbmv',113),('tzibjeqehq',91),('iipwhdbvvc',186),('eiyhgkwdww',165),('tdneietvri',145),('zesstzkhhs',159),('grobqmehts',115),('rhtcexnpkx',90),('sxjfhgsrko',87),
+('aqefjuekew',88),('zmynvnfedk',170),('efytejbnhv',170),('rrjdjgvzyc',86),('dwvuomqlnl',70),('whsouwrcli',138),('hqiixhuuup',150),('chrbpmsiwm',179),('wqbflyyjyl',52),('csiwhjaffq',192),
+('mvitciehjv',162),('tdeepuhvhm',172),('opyivldnfp',79),('shkxcxgfzt',172),('xoedrlliqe',97),('mzrpxmllhd',158),('hkbfyltoqh',109),('dnoovjofwe',155),('rzkrxvcijy',66),('hkfnhdcnue',131),
+('vrapnpngpl',115),('zffezvxexp',161),('ezllmdvjnb',160),('zkujmjjydd',68),('icpjufgcgb',164),('cooclsbwkq',51),('rkzekwmiau',139),('liijfaswkl',179),('cjydjqkrld',85),('pbuogzirqv',106),
+('yqqnwdkttn',153),('tcbkynnssl',116),('jpausbpvxe',199),('jegbttodxd',143),('ppoczbneiy',136),('szlhyhgrwa',69),('vuhcmbwuev',192),('zmjhkauaal',126),('jtylbsoftu',160),('znxovhikso',157),
+('cqqmhjnmzq',104),('jwvsaxvctz',124),('znftaihwhm',58),('lofrrbkbon',
+
+
+-- Step 4: (Assumed) Create index on the `name` column
+CREATE INDEX idx_name_first ON names(name(1));
+
+-- Step 5: Show the index from the `names` table
+SHOW INDEX FROM names;
