11707794c5b684dce959dbd8ce8ae6b74e87a1cc
Your Name <you@example.com>
2024-06-27 09:20:44 -0500

    a SQL script that creates an index idx_name_first on the table names and the first letter of name.
    Requirements:
    
    Import this table dump: names.sql.zip
    Only the first letter of name must be indexed

--- pipeline/databases/100-index_my_names.sql
+++ pipeline/databases/100-index_my_names.sql
@@ -1,12 +1,5 @@
 -- Import the table dump
 SOURCE names.sql;
 
--- Add a computed column for the first letter of the name
-ALTER TABLE names ADD COLUMN first_letter 
-CHAR(1) GENERATED ALWAYS 
-AS (LEFT(name, 1)) STORED;
-
--- Create an index on the computed column
-CREATE INDEX idx_name_first 
-ON names(first_letter)
-;
+-- Create an index on the first letter of the name
+CREATE INDEX idx_name_first ON names((LEFT(name, 1)));
